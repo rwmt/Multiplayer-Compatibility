c9c1d3d6b91324dabc4ebba84b602e9b50677354
notfood <notfood@users.noreply.github.com>
2019-07-04 11:08:51 -0500

    GiddyUp: Fix Battle Mounts combat

--- Source/Mods/GiddyUpBattleMounts.cs
+++ Source/Mods/GiddyUpBattleMounts.cs
@@ -1,4 +1,5 @@
-﻿using Harmony;
+﻿using System;
+using Harmony;
 using Multiplayer.API;
 using Verse;
 
@@ -6,16 +7,24 @@ namespace Multiplayer.Compat
 {
     /// <summary>Giddy Up! Battle Mounts</summary>
     /// <remarks>Suspect raids desync here</remarks>
-    /// <see href="https://github.com/rheirman/GiddyUpRideAndRoll"/>
-    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1331961995"/>
+    /// <see href="https://github.com/rheirman/battlemounts"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1217001091"/>
     [MpCompatFor("Giddy-up! Battle Mounts")]
-    public class GiddyUpBattleMounts
+    public class GiddyUpBattleMountsCompat
     {
-        public GiddyUpBattleMounts(ModContentPack mod)
+        public GiddyUpBattleMountsCompat(ModContentPack mod)
         {
             MP.RegisterSyncMethod(
                 AccessTools.Method("Battlemounts.Utilities.EnemyMountUtility:mountAnimals")
             );
+
+            Type type;
+            type = AccessTools.TypeByName("BattleMounts.Harmony.Jobdriver_Cleanup");
+            type = AccessTools.Inner(type, "JobDriver_Cleanup");
+            MpCompat.harmony.Patch(AccessTools.Method(type, "Prefix"),
+                prefix: new HarmonyMethod(typeof(GiddyUpBattleMountsCompat), nameof(StubItOut)));
         }
+
+        static bool StubItOut => false;
     }
 }
