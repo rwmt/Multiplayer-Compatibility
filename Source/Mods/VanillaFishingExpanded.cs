d63ab10a71577ccb43e84229730289bd6dd2f4a6
Cody-Spring <32400573+Cody-Spring@users.noreply.github.com>
2020-02-11 21:54:27 -0600

    Vanilla Fishing Expanded: Support Added
    Added support for Vanilla Fishing Expanded by Sarg Bjornson and Oskar Potocki
    https://steamcommunity.com/sharedfiles/filedetails/?id=1914064942

---
+++ Source/Mods/VanillaFishingExpanded.cs
@@ -0,0 +1,18 @@
+﻿using Harmony;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>VanillaCuisineExpanded-Fishing by juanosarg</summary>
+    /// <see href="https://github.com/juanosarg/VanillaCuisineExpanded-Fishing"/>
+    /// contribution to Multiplayer Compatibility by Cody Spring
+    [MpCompatFor("Vanilla Fishing Expanded")]
+    class VanillaFishingExpanded
+    {
+        public VanillaFishingExpanded(ModContentPack mod)
+        {
+            MP.RegisterSyncMethod(AccessTools.Method("VCE_Fishing.JobDriver_Fish:SelectFishToCatch"));
+        }
+    }
+}
