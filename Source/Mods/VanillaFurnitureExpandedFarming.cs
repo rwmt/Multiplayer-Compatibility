a3dd1946fd7d608ab978ccb3ac6a66f0b8a4497b
Cody-Spring <32400573+Cody-Spring@users.noreply.github.com>
2020-02-16 12:58:39 -0600

    Vanilla Furniture Expanded - Farming: Support Added

---
+++ Source/Mods/VanillaFurnitureExpandedFarming.cs
@@ -0,0 +1,34 @@
+﻿using Harmony;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Vanilla Furniture Expanded by Oskar Potocki and dninemfive</summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1957158779"/>
+    /// contribution to Multiplayer Compatibility by Cody Spring
+    [MpCompatFor("Vanilla Furniture Expanded - Farming")]
+    class VFEF
+    {
+        public VFEF(ModContentPack mod)
+        {
+            //RNG Fix
+            {
+                var methods = new[] {
+                    AccessTools.Method("VFEF.MoteSprinkler:NewMote"),
+                    AccessTools.Method("VFEF.MoteSprinkler:ThrowWaterSpray")
+                };
+
+                foreach (var method in methods)
+                {
+                    MpCompat.harmony.Patch(method,
+                        prefix: new HarmonyMethod(typeof(VFEF), nameof(FixRNGPre)),
+                        postfix: new HarmonyMethod(typeof(VFEF), nameof(FixRNGPos))
+                    );
+                }
+            }
+        }
+        static void FixRNGPre() => Rand.PushState();
+        static void FixRNGPos() => Rand.PopState();
+    }
+}
