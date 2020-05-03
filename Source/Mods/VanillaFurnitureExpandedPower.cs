ea9527135d89bae7ed79bab640adcb419d299ca4
SokyranTheDragon <exploder1531@gmail.com>
2020-05-03 15:52:41 +0200

    Fixed graphical error by patching methods later in initialization process

--- Source/Mods/VanillaFurnitureExpandedPower.cs
+++ Source/Mods/VanillaFurnitureExpandedPower.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Reflection.Emit;
-using HarmonyLib;
+﻿using HarmonyLib;
 using Multiplayer.API;
 using Verse;
 
@@ -15,55 +13,59 @@ namespace Multiplayer.Compat
         /// Contribution to Multiplayer Compatibility by Sokyran and Reshiram
         public VanillaPowerExpanded(ModContentPack mod)
         {
-            // Gizmos
+            LongEventHandler.ExecuteWhenFinished(() =>
             {
-                // Debug fill/empty
-                var type = AccessTools.TypeByName("VanillaPowerExpanded.CompPipeTank");
-                MP.RegisterSyncMethod(type, "<CompGetGizmosExtra>b__17_0");
-                MP.RegisterSyncMethod(type, "<CompGetGizmosExtra>b__17_1");
-
-                // Order to or cancel plugging the hole (method names shared by both types)
-                foreach (var typeName in new[] { "VanillaPowerExpanded.Building_GasGeyser", "VanillaPowerExpanded.Building_GasGeyser" })
+                // Gizmos
                 {
-                    type = AccessTools.TypeByName(typeName);
-                    MP.RegisterSyncMethod(type, "SetHoleForPlugging");
-                    MP.RegisterSyncMethod(type, "CancelHoleForPlugging");
+                    // Debug fill/empty
+                    var type = AccessTools.TypeByName("VanillaPowerExpanded.CompPipeTank");
+                    MP.RegisterSyncMethod(type, "<CompGetGizmosExtra>b__17_0");
+                    MP.RegisterSyncMethod(type, "<CompGetGizmosExtra>b__17_1");
+
+                    // Order to or cancel plugging the hole (method names shared by both types)
+                    foreach (var typeName in new[] { "VanillaPowerExpanded.Building_GasGeyser", "VanillaPowerExpanded.Building_GasGeyser" })
+                    {
+                        type = AccessTools.TypeByName(typeName);
+                        MP.RegisterSyncMethod(type, "SetHoleForPlugging");
+                        MP.RegisterSyncMethod(type, "CancelHoleForPlugging");
+                    }
                 }
-            }
 
-            // RNG Fix
-            {
-                var methods = new[] {
-                    AccessTools.Method("VanillaPowerExpanded.Building_SmallBattery:Tick"),
-                    AccessTools.Method("VanillaPowerExpanded.Building_SmallBattery:PostApplyDamage"),
-                    AccessTools.Method("VanillaPowerExpanded.WeatherEvent_CustomLightningStrike:FireEvent"),
-                    AccessTools.Method("VanillaPowerExpanded.MapComponentExtender:doMapSpawns"),
-                    AccessTools.Method("VanillaPowerExpanded.CompPlantHarmRadiusIfBroken:CompTick"),
-                    // HarmRandomPlantInRadius is only called by CompPlantHarmRadiusIfBroken:CompTick, no need for patching
-                    AccessTools.Method("VanillaPowerExpanded.CompPowerPlantNuclear:AffectCell"),
-                    AccessTools.Method("VanillaPowerExpanded.Building_GasGeyser:StartSpray"),
-                    AccessTools.Method("VanillaPowerExpanded.Building_Tank:Tick"), // Causes an error and a (rather minor) issue with graphics
-                    AccessTools.Method("VanillaPowerExpanded.Building_Tank:PostApplyDamage"), // Causes an error and a (rather minor) issue with graphics
-                    AccessTools.Method("VanillaPowerExpanded.GasExplosionUtility:TryStartFireNear"),
-                    AccessTools.Method("VanillaPowerExpanded.IncidentWorker_GasExplosion:TryExecuteWorker"),
-                    AccessTools.Method("VanillaPowerExpanded.IntermittentGasSprayer:SteamSprayerTick"),
-                    AccessTools.Method("VanillaPowerExpanded.IntermittentGasSprayer:ThrowAirPuffUp"),
-                    // NewBaseAirPuff is only called by IntermittentGasSprayer:ThrowAirPuffUp, no need for patching
-                    AccessTools.Method("VanillaPowerExpanded.GasPipeNet:PowerNetTick"),
-                    // PipeNetGrid pushes and pops all Rand calls, no need to patch
-                    AccessTools.Method("VanillaPowerExpanded.CompPowerAdvancedWater:PostSpawnSetup"), // Causes an error and issue with graphics
-                    // CompPowerAdvancedWater:RebuildCache is only calling a seeded random
-                    AccessTools.Method("VanillaPowerExpanded.CompPowerAdvancedWind:PostSpawnSetup"), // Causes an error and issue with graphics
-                };
-
-                foreach (var method in methods)
+                // RNG Fix
                 {
-                    MpCompat.harmony.Patch(method,
-                        prefix: new HarmonyMethod(typeof(VFEF), nameof(FixRNGPre)),
-                        postfix: new HarmonyMethod(typeof(VFEF), nameof(FixRNGPos))
-                    );
+                    var methods = new[]
+                    {
+                        AccessTools.Method("VanillaPowerExpanded.Building_SmallBattery:Tick"),
+                        AccessTools.Method("VanillaPowerExpanded.Building_SmallBattery:PostApplyDamage"),
+                        AccessTools.Method("VanillaPowerExpanded.WeatherEvent_CustomLightningStrike:FireEvent"),
+                        AccessTools.Method("VanillaPowerExpanded.MapComponentExtender:doMapSpawns"),
+                        AccessTools.Method("VanillaPowerExpanded.CompPlantHarmRadiusIfBroken:CompTick"),
+                        // HarmRandomPlantInRadius is only called by CompPlantHarmRadiusIfBroken:CompTick, no need for patching
+                        AccessTools.Method("VanillaPowerExpanded.CompPowerPlantNuclear:AffectCell"),
+                        AccessTools.Method("VanillaPowerExpanded.Building_GasGeyser:StartSpray"),
+                        AccessTools.Method("VanillaPowerExpanded.GasExplosionUtility:TryStartFireNear"),
+                        AccessTools.Method("VanillaPowerExpanded.IncidentWorker_GasExplosion:TryExecuteWorker"),
+                        AccessTools.Method("VanillaPowerExpanded.IntermittentGasSprayer:SteamSprayerTick"),
+                        AccessTools.Method("VanillaPowerExpanded.IntermittentGasSprayer:ThrowAirPuffUp"),
+                        // NewBaseAirPuff is only called by IntermittentGasSprayer:ThrowAirPuffUp, no need for patching
+                        AccessTools.Method("VanillaPowerExpanded.GasPipeNet:PowerNetTick"),
+                        // PipeNetGrid pushes and pops all Rand calls, no need to patch
+                        // CompPowerAdvancedWater:RebuildCache is only calling a seeded random
+                        AccessTools.Method("VanillaPowerExpanded.Building_Tank:Tick"), // Causes an error and a (rather minor) issue with graphics when patched early
+                        AccessTools.Method("VanillaPowerExpanded.Building_Tank:PostApplyDamage"), // Causes an error and a (rather minor) issue with graphics when patched early
+                        AccessTools.Method("VanillaPowerExpanded.CompPowerAdvancedWater:PostSpawnSetup"), // Causes an error and issue with graphics when patched early
+                        AccessTools.Method("VanillaPowerExpanded.CompPowerAdvancedWind:PostSpawnSetup"), // Causes an error and issue with graphics when patched early
+                    };
+
+                    foreach (var method in methods)
+                    {
+                        MpCompat.harmony.Patch(method,
+                            prefix: new HarmonyMethod(typeof(VFEF), nameof(FixRNGPre)),
+                            postfix: new HarmonyMethod(typeof(VFEF), nameof(FixRNGPos))
+                        );
+                    }
                 }
-            }
+            });
         }
 
         static void FixRNGPre() => Rand.PushState();
