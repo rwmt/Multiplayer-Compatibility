5de042a8644bd91593bd350ad3078d0a06f5d82f
SokyranTheDragon <exploder1531@gmail.com>
2020-05-03 15:48:28 +0200

    Added support for Vanilla Furniture Expanded - Security
    Using artillery for world map usage is still not working properly and needs more work.

---
+++ Source/Mods/VanillaFurnitureExpandedSecurity.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Vanilla Furniture Expanded - Power by Oskar Potocki, Trunken, and XeoNovaDan </summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1845154007"/>
+    /// <see href="https://github.com/AndroidQuazar/VanillaFurnitureExpanded-Security"/>
+    /// Contribution to Multiplayer Compatibility by Sokyran and Reshiram
+    [MpCompatFor("Vanilla Furniture Expanded - Security")]
+    class VFESecurity
+    {
+        public VFESecurity(ModContentPack mod)
+        {
+            LongEventHandler.ExecuteWhenFinished(() =>
+            {
+                // Artillery, mostly gizmos
+                {
+                    var type = AccessTools.TypeByName("VFESecurity.CompLongRangeArtillery");
+
+                    var methods = new[]
+                    {
+                        "StartChoosingTarget",
+                        "ResetForcedTarget",
+                        "SetTargetedTile",
+                        "ChooseWorldTarget",
+                    };
+
+                    foreach (var method in methods)
+                        MP.RegisterSyncMethod(type, method);
+                }
+
+                // RNG fix
+                {
+                    var methods = new MethodBase[]
+                    {
+                        AccessTools.Method("VFESecurity.ArtilleryStrikeArrivalAction_AIBase:Arrived"),
+                        AccessTools.Method("VFESecurity.ArtilleryStrikeArrivalAction_Insectoid:StrikeAction"),
+                        AccessTools.Method("VFESecurity.ArtilleryStrikeArrivalAction_Map:Arrived"),
+                        AccessTools.Method("VFESecurity.ArtilleryStrikeArrivalAction_Outpost:StrikeAction"),
+                        AccessTools.Method("VFESecurity.ArtilleryStrikeArrivalAction_PeaceTalks:Arrived"),
+                        AccessTools.Method("VFESecurity.ArtilleryStrikeArrivalAction_Settlement:StrikeAction"),
+                        AccessTools.Method("VFESecurity.ArtilleryComp:CompTick"),
+                        AccessTools.Method("VFESecurity.ArtilleryComp:TryStartBombardment"),
+                        AccessTools.Method("VFESecurity.WorldObjectCompProperties_Artillery:ArtilleryCountFor"),
+                        //AccessTools.Method("VFESecurity.Patch_Building_Trap.Spring:ShouldDestroy"),
+                        AccessTools.Method(AccessTools.Inner(AccessTools.TypeByName("VFESecurity.Patch_Building_Trap"), "Spring"), "ShouldDestroy"),
+                        // ArtilleryStrikeUtility:GetRandomShellFor and ArtilleryStrikeUtility:PotentialStrikeCells are only called by methods that are patched already
+                        AccessTools.Method("VFESecurity.Building_BarbedWire:SpringSub"),
+                        AccessTools.Method("VFESecurity.Building_TrapBear:SpringSub"),
+
+                        // This one seems like it should have no random calls at all in its hierarchy, but desync traces show that there are actually some.
+                        AccessTools.Method("VFESecurity.Verb_Dazzle:TryCastShot"),
+
+                        AccessTools.Method("VFESecurity.CompLongRangeArtillery:CompTick"),
+                        // ArtilleryComp:TryResolveArtilleryCount is called by ArtilleryComp:CompTick
+                        AccessTools.Method("VFESecurity.Building_Shield:Notify_EnergyDepleted"),
+                        AccessTools.Method("VFESecurity.Building_Shield:AbsorbDamage", new Type[] { typeof(float), typeof(DamageDef), typeof(float) }),
+                        AccessTools.Method("VFESecurity.Building_Shield:Draw"),
+
+                        // One of the desyncs points to the mote directly, but does not have any non-vanilla calls in its desync trace
+                        AccessTools.Constructor(AccessTools.TypeByName("Verse.Mote")),
+                    };
+
+                    foreach (var method in methods)
+                    {
+                        MpCompat.harmony.Patch(method,
+                            prefix: new HarmonyMethod(typeof(VFEF), nameof(FixRNGPre)),
+                            postfix: new HarmonyMethod(typeof(VFEF), nameof(FixRNGPos))
+                        );
+                    }
+                }
+            });
+        }
+
+        static void FixRNGPre() => Rand.PushState();
+        static void FixRNGPos() => Rand.PopState();
+    }
+}
