86e75c3e511e239c8c3bea0e3bd1b81339d531e6
SokyranTheDragon <exploder1531@gmail.com>
2020-05-17 03:25:35 +0200

    Added fish size change gizmo for fishing zones

--- Source/Mods/VanillaFishingExpanded.cs
+++ Source/Mods/VanillaFishingExpanded.cs
@@ -1,4 +1,7 @@
-﻿using HarmonyLib;
+﻿using System;
+using System.Reflection;
+using System.Runtime.Serialization;
+using HarmonyLib;
 using Multiplayer.API;
 using Verse;
 
@@ -10,13 +13,48 @@ namespace Multiplayer.Compat
     [MpCompatFor("VanillaExpanded.VCEF")]
     class VanillaFishingExpanded
     {
+        private static Type commandType;
+        private static FieldInfo mapField;
+        private static FieldInfo fishingZoneField;
+
         public VanillaFishingExpanded(ModContentPack mod)
         {
-            var method = AccessTools.Method("VCE_Fishing.JobDriver_Fish:SelectFishToCatch");
+            // RNG fix
+            {
+                var method = AccessTools.Method("VCE_Fishing.JobDriver_Fish:SelectFishToCatch");
 
-            PatchingUtilities.PatchSystemRand(method);
+                PatchingUtilities.PatchSystemRand(method);
 
-            MP.RegisterSyncMethod(method);
+                MP.RegisterSyncMethod(method);
+            }
+
+            // Gizmo (select fish size to catch)
+            {
+                commandType = AccessTools.TypeByName("VCE_Fishing.Command_SetFishList");
+                mapField = AccessTools.Field(commandType, "map");
+                fishingZoneField = AccessTools.Field(commandType, "zone");
+
+                MP.RegisterSyncMethod(commandType, "<ProcessInput>b__4_0");
+                MP.RegisterSyncMethod(commandType, "<ProcessInput>b__4_1");
+                MP.RegisterSyncMethod(commandType, "<ProcessInput>b__4_2");
+
+                MP.RegisterSyncWorker<Command>(SyncFishingZoneChange, commandType, shouldConstruct: false);
+            }
+        }
+
+        private static void SyncFishingZoneChange(SyncWorker sync, ref Command command)
+        {
+            if (sync.isWriting)
+            {
+                sync.Write(((Map)mapField.GetValue(command)).Index);
+                sync.Write((Zone)fishingZoneField.GetValue(command));
+            }
+            else
+            {
+                command = (Command)FormatterServices.GetUninitializedObject(commandType);
+                mapField.SetValue(command, Find.Maps[sync.Read<int>()]);
+                fishingZoneField.SetValue(command, sync.Read<Zone>());
+            }
         }
     }
 }
