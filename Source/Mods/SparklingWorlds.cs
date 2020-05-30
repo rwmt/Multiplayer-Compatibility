ee0da2433d0c4442cbdc240cb718dbbb434d0990
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2020-05-31 00:59:52 +0200

    Sparkling Worlds support (#17)

---
+++ Source/Mods/SparklingWorlds.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat.Mods
+{
+    /// <summary>PowerSwitch by Albion</summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1123043922"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1195241161"/>
+    [MpCompatFor("Albion.SparklingWorlds.Full")]
+    [MpCompatFor("Albion.SparklingWorlds.Core")]
+    public class SparklingWorlds
+    {
+        public SparklingWorlds(ModContentPack mod)
+        {
+            LongEventHandler.ExecuteWhenFinished(() => MP.RegisterSyncMethod(AccessTools.TypeByName("SparklingWorlds.CompOrbitalLaunchSW"), "TryLaunch"));
+        }
+    }
+}
