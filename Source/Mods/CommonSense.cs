a26d39c9935434ae9008b1a1db1dacc3933ebdef
SokyranTheDragon <exploder1531@gmail.com>
2020-07-15 00:37:02 +0200

    Synced unload button in Common Sense

---
+++ Source/Mods/CommonSense.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using Multiplayer.API;
+using Multiplayer.Compat;
+using Verse;
+
+namespace Multiplayer_Compat.Mods
+{
+    /// <summary>Common Sense by avil</summary>
+    /// <see href="https://github.com/catgirlfighter/RimWorld_CommonSense"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1561769193"/>
+    [MpCompatFor("avilmask.CommonSense")]
+    class CommonSense
+    {
+        private static ISyncField shouldUnloadSyncField;
+        private static FieldInfo manualUnloadEnabledField;
+        private static MethodInfo getCompUnlockerCheckerMethod;
+
+        public CommonSense(ModContentPack mod)
+        {
+            manualUnloadEnabledField = AccessTools.Field(AccessTools.TypeByName("CommonSense.Settings"), "gui_manual_unload");
+            var type = AccessTools.TypeByName("CommonSense.CompUnloadChecker");
+            MP.RegisterSyncWorker<ThingComp>(SyncComp, type);
+            shouldUnloadSyncField = MP.RegisterSyncField(AccessTools.Field(type, "ShouldUnload"));
+            getCompUnlockerCheckerMethod = AccessTools.Method(type, "GetChecker");
+
+            MpCompat.harmony.Patch(AccessTools.Method("CommonSense.ITab_Pawn_Gear_DrawThingRow_CommonSensePatch:Prefix"), 
+                prefix: new HarmonyMethod(typeof(CommonSense), nameof(CommonSensePatchPrefix)),
+                postfix: new HarmonyMethod(typeof(CommonSense), nameof(CommonSensePatchPostix)));
+        }
+
+        private static void CommonSensePatchPrefix(Thing thing)
+        {
+            if (MP.IsInMultiplayer && (bool)manualUnloadEnabledField.GetValue(null))
+            {
+                MP.WatchBegin();
+                var comp = getCompUnlockerCheckerMethod.Invoke(null, new object[] { thing, false, false });
+                shouldUnloadSyncField.Watch(comp);
+            }
+        }
+
+        private static void CommonSensePatchPostix()
+        {
+            if (MP.IsInMultiplayer && (bool)manualUnloadEnabledField.GetValue(null))
+                MP.WatchEnd();
+        }
+
+        private static void SyncComp(SyncWorker sync, ref ThingComp thing)
+        {
+            if (sync.isWriting)
+                sync.Write<Thing>(thing.parent);
+            else
+                thing = (ThingComp)getCompUnlockerCheckerMethod.Invoke(null, new object[] { sync.Read<Thing>(), false, false });
+        }
+    }
+}
