ff0b63b7845f33eb559546381b5dc86c59d775c7
Thomas107500 <hotsunngai@gmail.com>
2020-07-16 15:54:12 +0800

    Create RunandGun.cs

---
+++ Source/Mods/RunandGun.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Verse;
+using HarmonyLib;
+using Multiplayer.API;
+
+namespace Multiplayer.Compat
+{
+    [MpCompatFor("roolo.RunAndGun")]
+    class RunandGun
+    {
+        Type type = AccessTools.TypeByName("RunAndGun.Harmony.Pawn_DraftController_GetGizmos_Patch");
+        public RunandGun(ModContentPack mod)
+            {
+                MP.RegisterSyncDelegate(type, "<>c__DisplayClass0_0", "<Postfix>b__1");
+            }
+    }
+
+
+}
