652a7268a852b5e952748293b5d2252d5a898c26
Thomas107500 <49201909+Thomas107500@users.noreply.github.com>
2020-07-17 00:13:36 +0800

    RunAndGun Multiplayer Compatibility (#25)

---
+++ Source/Mods/RunandGun.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Verse;
+using HarmonyLib;
+using Multiplayer.API;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>RunAndGun by roolo</summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1204108550"/>
+    [MpCompatFor("roolo.RunAndGun")]
+    class RunandGun
+    {
+        public RunandGun(ModContentPack mod)
+            {
+                Type type = AccessTools.TypeByName("RunAndGun.Harmony.Pawn_DraftController_GetGizmos_Patch");
+                
+                MP.RegisterSyncDelegate(type, "<>c__DisplayClass0_0", "<Postfix>b__1");
+            }
+    }
+}
