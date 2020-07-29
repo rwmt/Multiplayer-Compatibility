27066945f6c517030f9c596ca86b54da611c2649
Thomas107500 <49201909+Thomas107500@users.noreply.github.com>
2020-07-30 02:33:49 +0800

    Update MiscRobots.cs (#33)

--- Source/Mods/MiscRobots.cs
+++ Source/Mods/MiscRobots.cs
@@ -10,7 +10,8 @@ namespace Multiplayer.Compat
     [MpCompatFor("Haplo.Miscellaneous.Robots")]
     class MiscRobots
     {
-        public MiscRobots(ModContentPack mod)
+        public MiscRobots(ModContentPack mod) => LongEventHandler.ExecuteWhenFinished(LatePatch);
+        private static void LatePatch()
         {
             Type rechargestationType = AccessTools.TypeByName("AIRobot.X2_Building_AIRobotRechargeStation");
             Type robotType = AccessTools.TypeByName("AIRobot.X2_AIRobot");
@@ -22,12 +23,16 @@ namespace Multiplayer.Compat
             MP.RegisterSyncMethod(rechargestationType, "Button_ResetDestroyedRobot");
             MP.RegisterSyncMethod(rechargestationType, "Button_SpawnAllAvailableBots");
             MP.RegisterSyncMethod(rechargestationType, "Button_SpawnBot");
-            
+
             //Might as well sync the arrow gizmos in case some one actually click on it, because it is accessible by users,but not affecting gameplay
             MP.RegisterSyncMethod(robotType, "Debug_ForceGotoDistance");
-            
+
             MP.RegisterSyncMethod(rechargestationType, "AddRobotToContainer").SetContext(SyncContext.CurrentMap);
+
+            PatchingUtilities.PatchPushPopRand("AIRobot.MoteThrowHelper:ThrowBatteryXYZ");
+            PatchingUtilities.PatchPushPopRand("AIRobot.MoteThrowHelper:ThrowNoRobotSign");
+
         }
-        
+
     }
 }
