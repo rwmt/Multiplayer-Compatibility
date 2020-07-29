4490877b4a10a5753f10947e8bab038257a53020
Thomas107500 <49201909+Thomas107500@users.noreply.github.com>
2020-07-30 02:33:49 +0800

    Update MiscRobots.cs (#33) (#35)

--- Source/Mods/MiscRobots.cs
+++ Source/Mods/MiscRobots.cs
@@ -1,6 +1,8 @@
 ﻿using HarmonyLib;
 using Multiplayer.API;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using Verse;
 
 namespace Multiplayer.Compat
@@ -10,11 +12,13 @@ namespace Multiplayer.Compat
     [MpCompatFor("Haplo.Miscellaneous.Robots")]
     class MiscRobots
     {
-        public MiscRobots(ModContentPack mod)
+        public MiscRobots(ModContentPack mod) => LongEventHandler.ExecuteWhenFinished(LatePatch);
+        private static void LatePatch()
         {
             Type rechargestationType = AccessTools.TypeByName("AIRobot.X2_Building_AIRobotRechargeStation");
             Type robotType = AccessTools.TypeByName("AIRobot.X2_AIRobot");
-
+            Type robothelperType = AccessTools.TypeByName("AIRobot.AIRobot_Helper");
+            
             MP.RegisterSyncMethod(rechargestationType, "Button_CallAllBotsForShutdown");
             MP.RegisterSyncMethod(rechargestationType, "Button_CallBotForShutdown");
             MP.RegisterSyncMethod(rechargestationType, "Button_RepairDamagedRobot");
@@ -22,12 +26,17 @@ namespace Multiplayer.Compat
             MP.RegisterSyncMethod(rechargestationType, "Button_ResetDestroyedRobot");
             MP.RegisterSyncMethod(rechargestationType, "Button_SpawnAllAvailableBots");
             MP.RegisterSyncMethod(rechargestationType, "Button_SpawnBot");
-            
+
             //Might as well sync the arrow gizmos in case some one actually click on it, because it is accessible by users,but not affecting gameplay
             MP.RegisterSyncMethod(robotType, "Debug_ForceGotoDistance");
-            
+
             MP.RegisterSyncMethod(rechargestationType, "AddRobotToContainer").SetContext(SyncContext.CurrentMap);
+
+            PatchingUtilities.PatchPushPopRand("AIRobot.MoteThrowHelper:ThrowBatteryXYZ");
+            PatchingUtilities.PatchPushPopRand("AIRobot.MoteThrowHelper:ThrowNoRobotSign");
+
+            MP.RegisterSyncMethod(robothelperType, "StartStationRepairJob");
+
         }
-        
     }
 }
