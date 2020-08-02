89d8ed6470ce00dc0bc3fe78af4d4794262619f2
Thomas107500 <49201909+Thomas107500@users.noreply.github.com>
2020-08-03 00:17:05 +0800

    Compatibility for MiscTraining (#36)

---
+++ Source/Mods/MiscTraining.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Misc. Training by HaploX1</summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=717575199"/>
+    [MpCompatFor("Haplo.Miscellaneous.Training")]
+    class MiscTraining
+    {
+        public MiscTraining(ModContentPack mod)
+        {
+            PatchingUtilities.PatchPushPopRand("TrainingFacility.JobDriver_Archery:ShootArrow");
+        }
+    }
+}
