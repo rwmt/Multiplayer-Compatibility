401c677b837da1e4a8dc1ce90aabfc3c9376f1b2
SokyranTheDragon <exploder1531@gmail.com>
2020-10-14 21:49:36 +0200

    Compat for Deep Storage

---
+++ Source/Mods/DeepStorage.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Deep Storage by LWM</summary>
+    /// <see href="https://github.com/lilwhitemouse/RimWorld-LWM.DeepStorage"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1617282896"/>
+    [MpCompatFor("LWM.DeepStorage")]
+    class DeepStorage
+    {
+        public DeepStorage(ModContentPack mod) 
+            => MP.RegisterSyncMethod(AccessTools.TypeByName("LWM.DeepStorage.ITab_DeepStorage_Inventory"), "EjectTarget").CancelIfAnyArgNull();
+    }
+}
