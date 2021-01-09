bcb76f3239ad487bb9ed1eff2c0412ca50cfcf44
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2021-01-09 15:33:17 +0100

    Compatibility for Vanilla Weapons Expanded - Laser

---
+++ Source/Mods/VanillaWeaponsLaser.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Vanilla Weapons Expanded - Laser by Oskar Potocki, Kikohi, Primus the Conqueror, Ogliss, AUTOMATIC, Jecrell</summary>
+    /// <see href="https://github.com/AndroidQuazar/VanillaWeaponsExpanded-Laser"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1989352844"/>
+    [MpCompatFor("VanillaExpanded.VWEL")]
+    public class VanillaWeaponsLaser
+    {
+        public VanillaWeaponsLaser(ModContentPack mod)
+        {
+            var type = AccessTools.TypeByName("VanillaWeaponsExpandedLaser.CompLaserCapacitor");
+            MpCompat.RegisterSyncMethodByIndex(type, "<CompGetGizmosExtra>", 1);
+        }
+    }
+}
