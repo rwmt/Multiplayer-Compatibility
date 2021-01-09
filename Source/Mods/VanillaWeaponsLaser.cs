aa7df2c188eb75136ed22e6bdbc8fa9a50fcc895
SokyranTheDragon <exploder1531@gmail.com>
2021-01-09 15:05:15 +0100

    Patched Vanilla Weapons Expanded - Laser

---
+++ Source/Mods/VanillaWeaponsLaser.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    [MpCompatFor("VanillaExpanded.VWEL")]
+    public class VanillaWeaponsLaser
+    {
+        public VanillaWeaponsLaser(ModContentPack mod)
+        {
+            var type = AccessTools.TypeByName("VanillaWeaponsExpandedLaser.CompLaserCapacitor");
+            MpCompat.RegisterSyncMethodByIndex(type, "<CompGetGizmosExtra>", 1);
+        }
+    }
+}
