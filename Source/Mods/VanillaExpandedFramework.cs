e33f3283df9372d6cebc8a0a11df2828ccd37efb
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2021-01-13 04:02:04 +0100

    Updated Vanilla Expanded Framework to match most recent version (#55)

--- Source/Mods/VanillaExpandedFramework.cs
+++ Source/Mods/VanillaExpandedFramework.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using HarmonyLib;
@@ -14,21 +15,24 @@ namespace Multiplayer.Compat
     [MpCompatFor("OskarPotocki.VanillaFactionsExpanded.Core")]
     class VanillaExpandedFramework
     {
-        private static FieldInfo itemProcessorField;
+        private static FieldInfo setStoneBuildingField;
 
         public VanillaExpandedFramework(ModContentPack mod)
         {
             var type = AccessTools.TypeByName("ItemProcessor.Building_ItemProcessor");
-            // _1 and _5 are used to check if gizmo should be enabled, so we don't sync them
-            MpCompat.RegisterSyncMethodsByIndex(type, "<GetGizmos>", 0, 2, 3, 4, 6, 7);
+            // _1, _5 and _7 are used to check if gizmo should be enabled, so we don't sync them
+            MpCompat.RegisterSyncMethodsByIndex(type, "<GetGizmos>", 0, 2, 3, 4, 6, 8, 9, 10);
 
             type = AccessTools.TypeByName("ItemProcessor.Command_SetQualityList");
-            itemProcessorField = AccessTools.Field(type, "building");
-            MP.RegisterSyncWorker<Command>(SyncSetTargetQuality, type, shouldConstruct: true);
+            MP.RegisterSyncWorker<Command>(SyncCommandWithBuilding, type, shouldConstruct: true);
             MpCompat.RegisterSyncMethodsByIndex(type, "<ProcessInput>", Enumerable.Range(0, 8).ToArray());
 
+            type = AccessTools.TypeByName("ItemProcessor.Command_SetOutputList");
+            MP.RegisterSyncWorker<Command>(SyncCommandWithBuilding, type, shouldConstruct: true);
+            MP.RegisterSyncMethod(type, "TryConfigureIngredientsByOutput");
+
             // Keep an eye on this in the future, seems like something the devs could combine into a single class at some point
-            foreach (var ingredientNumber in new[] { "First", "Second", "Third" })
+            foreach (var ingredientNumber in new[] { "First", "Second", "Third", "Fourth" })
             {
                 type = AccessTools.TypeByName($"ItemProcessor.Command_Set{ingredientNumber}ItemList");
                 MP.RegisterSyncWorker<Command>(SyncSetIngredientCommand, type, shouldConstruct: true);
@@ -38,14 +42,35 @@ namespace Multiplayer.Compat
 
             // AddHediff desyncs with Arbiter, but seems fine without it
             PatchingUtilities.PatchPushPopRand("VanillaCookingExpanded.Thought_Hediff:MoodOffset");
+
+            type = AccessTools.TypeByName("VFECore.CompPawnDependsOn");
+            MpCompat.RegisterSyncMethodByIndex(type, "<CompGetGizmosExtra>", 0).SetDebugOnly();
+
+            type = AccessTools.TypeByName("VanillaFurnitureExpanded.CompConfigurableSpawner");
+            MpCompat.RegisterSyncMethodByIndex(type, "<CompGetGizmosExtra>", 0).SetDebugOnly();
+
+            type = AccessTools.TypeByName("VanillaFurnitureExpanded.Command_SetItemsToSpawn");
+            MP.RegisterSyncDelegate(type, "<>c__DisplayClass2_0", "<ProcessInput>b__1");
+
+            type = AccessTools.TypeByName("VanillaFurnitureExpanded.CompRockSpawner");
+            MpCompat.RegisterSyncMethodByIndex(type, "<CompGetGizmosExtra>", 0);
+
+            type = AccessTools.TypeByName("VanillaFurnitureExpanded.Command_SetStoneType");
+            setStoneBuildingField = AccessTools.Field(type, "building");
+            MpCompat.RegisterSyncMethodByIndex(type, "<ProcessInput>", 0);
+            MP.RegisterSyncWorker<Command>(SyncSetStoneTypeCommand, type, shouldConstruct: true);
+            MP.RegisterSyncDelegate(type, "<>c__DisplayClass2_0", "<ProcessInput>b__1");
         }
 
-        private static void SyncSetTargetQuality(SyncWorker sync, ref Command command)
+        private static void SyncCommandWithBuilding(SyncWorker sync, ref Command command)
         {
+            var traverse = Traverse.Create(command);
+            var building = traverse.Field("building");
+
             if (sync.isWriting)
-                sync.Write(itemProcessorField.GetValue(command) as Thing);
+                sync.Write(building.GetValue() as Thing);
             else
-                itemProcessorField.SetValue(command, sync.Read<Thing>());
+                building.SetValue(sync.Read<Thing>());
         }
 
         private static void SyncSetIngredientCommand(SyncWorker sync, ref Command command)
@@ -57,20 +82,21 @@ namespace Multiplayer.Compat
             if (sync.isWriting)
             {
                 sync.Write(building.GetValue() as Thing);
-                var list = ingredientList.GetValue() as List<Thing>;
-                sync.Write(list.Count);
-                foreach (var item in list)
-                    sync.Write(item as Thing);
+                sync.Write(ingredientList.GetValue() as List<Thing>);
             }
             else
             {
                 building.SetValue(sync.Read<Thing>());
-                int count = sync.Read<int>();
-                var list = new List<Thing>(count);
-                for (int i = 0; i < count; i++)
-                    list.Add(sync.Read<Thing>());
-                ingredientList.SetValue(list);
+                ingredientList.SetValue(sync.Read<List<Thing>>());
             }
+        }
+
+        private static void SyncSetStoneTypeCommand(SyncWorker sync, ref Command obj)
+        {
+            if (sync.isWriting)
+                sync.Write(setStoneBuildingField.GetValue(obj) as ThingComp);
+            else
+                setStoneBuildingField.SetValue(obj, sync.Read<ThingComp>());
         }
     }
 }
