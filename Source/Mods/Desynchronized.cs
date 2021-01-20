bae3767a458cf7478d328d844a897ff84b816688
Ari <49175620+AriAlavi@users.noreply.github.com>
2021-01-20 14:09:55 -0800

    Added compatibility for Desynchronized (#50)

---
+++ Source/Mods/Desynchronized.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Desynchronized by Vectorial1024s and emipa606</summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2222607126"/>
+    /// contribution to Multiplayer Compatibility by Ari
+    [MpCompatFor("Mlie.Desynchronized")]
+    public class Desynchronized
+    {
+        public Desynchronized(ModContentPack mod)
+        {
+            var rngFixMethods = new[]
+            {
+                "Desynchronized.TNDBS.Pawn_NewsKnowledgeTracker:ForgetRandom",
+                "Desynchronized.TNDBS.Pawn_NewsKnowledgeTracker:ForgetRandomly",
+                "Desynchronized.TNDBS.Utilities.NewsSpreadUtility:SelectNewsRandomly",
+                "Desynchronized.TNDBS.Utilities.NewsSpreadUtility:SelectNewsDistinctly",
+                "Desynchronized.Patches.NewsTransmit.PostFix_InteractionWorker:ExecuteNewsTarnsmission",
+                "Desynchronized.TNDBS.TaleNewsPawnDied:CalculateNewsImportanceForPawn",
+            };
+            PatchingUtilities.PatchPushPopRand(rngFixMethods);
+        }
+    }
+}
