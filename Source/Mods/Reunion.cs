b9011c07efd98532999c421888ba7ce704a06ee7
mjyousif <33187058+mjyousif@users.noreply.github.com>
2021-01-27 11:29:53 -0600

    Add compatibility for Reunion (#59)

---
+++ Source/Mods/Reunion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Reunion by Kyrun</summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1985186461"/>
+    /// <see href="https://github.com/kyrun/rimworld-reunion"/>
+    [MpCompatFor("Kyrun.Reunion")]
+    public class Reunion
+    {
+        public Reunion(ModContentPack mod)
+        {
+            var methods = new[]
+            {
+                "Kyrun.Reunion.GameComponent:GetRandomAllyForSpawning",
+                "Kyrun.Reunion.GameComponent:TryScheduleNextEvent",
+                "Kyrun.Reunion.GameComponent:DecideAndDoEvent",
+            };
+            PatchingUtilities.PatchUnityRand(methods);
+        }
+    }
+}
