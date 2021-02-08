f908d6c8007c796e7d4d6f4d0f9b0f7d6c792379
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2021-02-08 05:14:59 +0100

    MP compat for Netrve's DeepStorage GUI (#49)

---
+++ Source/Mods/DeepStorageGUI.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Netrve's DeepStorage GUI by Netrve</summary>
+    /// <see href="https://github.com/Dakraid/RW_DSGUI/"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2169841018"/>
+    [MpCompatFor("netrve.dsgui")]
+    class DeepStorageGUI
+    {
+        public DeepStorageGUI(ModContentPack mod)
+            => MP.RegisterSyncMethod(AccessTools.TypeByName("DSGUI.DSGUI_TabItem"), "EjectTarget").CancelIfAnyArgNull();
+    }
+}
