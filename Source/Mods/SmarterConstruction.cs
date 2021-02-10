b978bbb9431b5cf1961b2465292bf7830be2d1b1
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2021-02-10 19:44:51 +0100

    Added compatibility for Smarter Construction (#64)

---
+++ Source/Mods/SmarterConstruction.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Smarter Construction by Hultis</summary>
+    /// <see href="https://github.com/dhultgren/rimworld-smarter-construction"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2202185773"/>
+    [MpCompatFor("dhultgren.smarterconstruction")]
+    class SmarterConstruction
+    {
+        public SmarterConstruction(ModContentPack mod)
+        {
+            var type = AccessTools.TypeByName("SmarterConstruction.Patches.Patch_WorkGiver_Scanner_GetPriority");
+            var field = AccessTools.Field(type, "random");
+
+            field.SetValue(null, new PatchingUtilities.RandRedirector());
+        }
+    }
+}
