148cd7ed54b39a16d8a49b3c1f0dfe6dfb316ae4
SokyranTheDragon <exploder1531@gmail.com>
2021-02-10 18:32:35 +0100

    What The Hack RNG patch

---
+++ Source/Mods/WhatTheHack.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    [MpCompatFor("roolo.whatthehack")]
+    class WhatTheHack
+    {
+        public WhatTheHack(ModContentPack mod)
+        {
+            var methods = new[]
+            {
+                AccessTools.Method(AccessTools.TypeByName("WhatTheHack.Harmony.IncidentWorker_Raid_TryExecuteWorker"), "SpawnHackedMechanoids"),
+                AccessTools.Method(AccessTools.TypeByName("WhatTheHack.Harmony.Pawn_JobTracker_DetermineNextJob"), "HackedPoorlyEvent"),
+                AccessTools.Method(AccessTools.TypeByName("WhatTheHack.Harmony.Thing_ButcherProducts"), "GenerateExtraButcherProducts"),
+                AccessTools.Method(AccessTools.TypeByName("WhatTheHack.Needs.Need_Maintenance"), "MaybeUnhackMechanoid"),
+                AccessTools.Method(AccessTools.TypeByName("WhatTheHack.Recipes.Recipe_Hacking"), "CheckHackingFail"),
+            };
+
+            PatchingUtilities.PatchSystemRand(methods, false);
+        }
+    }
+}
