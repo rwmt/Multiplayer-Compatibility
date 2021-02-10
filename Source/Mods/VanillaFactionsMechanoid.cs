bf69d001b629989031de3168ed669be51b25a9a8
SokyranTheDragon <exploder1531@gmail.com>
2021-02-10 18:34:17 +0100

    Fix warning caused by patch for Vanilla Factions Expanded - Mechanoids

--- Source/Mods/VanillaFactionsMechanoid.cs
+++ Source/Mods/VanillaFactionsMechanoid.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using HarmonyLib;
+﻿using HarmonyLib;
 using Multiplayer.API;
 using Verse;
 
@@ -13,17 +12,17 @@ namespace Multiplayer.Compat
     {
         public VanillaFactionsMechanoid(ModContentPack mod)
         {
-            var type = AccessTools.TypeByName("VFEMech.MissileSilo");
-            MP.RegisterSyncMethod(type, "StartFire");
-            MP.RegisterSyncMethod(type, "ConfigureNewTarget");
-            foreach (var method in MpCompat.RegisterSyncMethodsByIndex(type, "<GetGizmos>", 2, 3)) method.SetDebugOnly();
-
             LongEventHandler.ExecuteWhenFinished(LatePatch);
         }
 
         public static void LatePatch()
         {
-            var type = AccessTools.TypeByName("VFE.Mechanoids.Buildings.Building_AutoPlant");
+            var type = AccessTools.TypeByName("VFEMech.MissileSilo");
+            MP.RegisterSyncMethod(type, "StartFire");
+            MP.RegisterSyncMethod(type, "ConfigureNewTarget");
+            foreach (var method in MpCompat.RegisterSyncMethodsByIndex(type, "<GetGizmos>", 2, 3)) method.SetDebugOnly();
+
+            type = AccessTools.TypeByName("VFE.Mechanoids.Buildings.Building_AutoPlant");
             // Methods b__8_0, b__8_2, b__8_3. They are the first 3 methods in the file. The b__8_1 is the last one, and thus has index 3.
             MpCompat.RegisterSyncMethodsByIndex(type, "<GetGizmos>", 0, 1, 2);
         }
