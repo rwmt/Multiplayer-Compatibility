33e8357af67d2132d3f52342394b30f9bf06c96c
SokyranTheDragon <exploder1531@gmail.com>
2021-02-18 19:57:23 +0100

    Added compatibility for Vanilla Brewing Expanded and submods
    Indirectly fixes RNG desync in Vanilla Brewing Expanded - Coffees and Teas

---
+++ Source/Mods/VanillaBrewingExpanded.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary> Vanilla Brewing Expanded by Oskar Potocki, Sarg Bjornson, Chowder</summary>
+    /// <see href="https://steamcommunity.com/workshop/filedetails/?id=2186560858"/>
+    /// <see href="https://github.com/juanosarg/VanillaBrewingExpanded"/>
+    [MpCompatFor("VanillaExpanded.VBrewE")]
+    class VanillaBrewingExpanded
+    {
+        public VanillaBrewingExpanded(ModContentPack mod)
+        {
+            PatchingUtilities.PatchSystemRand("VanillaBrewingExpanded.Plant_AutoProduce:TickLong");
+            PatchingUtilities.PatchSystemRandCtor("VanillaBrewingExpanded.Hediff_ConsumedCocktail");
+        }
+    }
+}
