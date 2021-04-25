78327b1baf4ac26ed6947b6e72f6a5d0d4f28036
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2021-04-25 22:16:46 +0200

    Compat for MinifyEverything (#91)

---
+++ Source/Mods/MinifyEverything.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary></summary>
+    /// <see href="https://github.com/erdelf/MinifyEverything"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=872762753"/>
+    [MpCompatFor("erdelf.MinifyEverything")]
+    public class MinifyEverything
+    {
+        public MinifyEverything(ModContentPack mod) => LongEventHandler.ExecuteWhenFinished(LatePatch);
+
+        public static void LatePatch()
+        {
+            var type = AccessTools.TypeByName("MinifyEverything.MinifyEverything");
+
+            MpCompat.harmony.Patch(AccessTools.Method(type, "DoStuff"),
+                prefix: new HarmonyMethod(typeof(MinifyEverything), nameof(PreDoStuff)));
+        }
+
+        // Cancel the coroutine in MP and redirect the action to LongEventHandler
+        private static bool PreDoStuff(Action action, ref IEnumerator __result)
+        {
+            if (!MP.IsInMultiplayer) return true;
+            // Return a fake, empty coroutine as a result, so we don't get any errors
+            __result = FakeCoroutine();
+            LongEventHandler.ExecuteWhenFinished(action);
+            return false;
+        }
+
+        private static IEnumerator FakeCoroutine()
+        {
+            yield return null;
+        }
+    }
+}
