f180de5caea20755746ac52790ff991ca5833b20
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2021-05-05 19:31:08 +0200

    Patch Missile Silo from Vanilla Factions Expanded - Mechanoids (#83)

--- Source/Mods/VanillaFactionsMechanoid.cs
+++ Source/Mods/VanillaFactionsMechanoid.cs
@@ -10,21 +10,30 @@ namespace Multiplayer.Compat
     [MpCompatFor("OskarPotocki.VFE.Mechanoid")]
     public class VanillaFactionsMechanoid
     {
-        public VanillaFactionsMechanoid(ModContentPack mod)
-        {
-            LongEventHandler.ExecuteWhenFinished(LatePatch);
-        }
+        public VanillaFactionsMechanoid(ModContentPack mod) => LongEventHandler.ExecuteWhenFinished(LatePatch);
 
-        public static void LatePatch()
+        private static void LatePatch()
         {
             var type = AccessTools.TypeByName("VFEMech.MissileSilo");
             MP.RegisterSyncMethod(type, "StartFire");
-            MP.RegisterSyncMethod(type, "ConfigureNewTarget");
+            var configureNewTargetMethod = AccessTools.Method(type, "ConfigureNewTarget");
+            MP.RegisterSyncMethod(configureNewTargetMethod);
             foreach (var method in MpCompat.RegisterSyncMethodsByIndex(type, "<GetGizmos>", 2, 3)) method.SetDebugOnly();
+
+            MpCompat.harmony.Patch(configureNewTargetMethod,
+                postfix: new HarmonyMethod(typeof(VanillaFactionsMechanoid), nameof(CloseWorldTargetter)));
 
             type = AccessTools.TypeByName("VFE.Mechanoids.Buildings.Building_AutoPlant");
             // Methods b__8_0, b__8_2, b__8_3. They are the first 3 methods in the file. The b__8_1 is the last one, and thus has index 3.
             MpCompat.RegisterSyncMethodsByIndex(type, "<GetGizmos>", 0, 1, 2);
+        }
+
+        private static void CloseWorldTargetter(bool __result)
+        {
+            // Force close the targetter to give the players a visual cue that it was successful
+            // Otherwise, it would succesfully mark the target but not give any indication
+            if (MP.IsInMultiplayer && __result && Find.WorldTargeter.IsTargeting && Find.WorldTargeter.closeWorldTabWhenFinished)
+                Find.WorldTargeter.StopTargeting();
         }
     }
 }
