fdbb012898ff4e1a3c852bb9b12cc0b31ca01bb4
SokyranTheDragon <exploder1531@gmail.com>
2021-08-14 02:10:42 +0200

    Compat for Clutter Structures

---
+++ Source/Mods/ClutterStructures.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Clutter Structures by mrofa and neronix17</summary>
+    /// <see href="https://github.com/neronix17/-O21-Clutter-Structure"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2008681009"/>
+    [MpCompatFor("neronix17.clutterstructures")]
+    class ClutterStructures
+    {
+        public ClutterStructures(ModContentPack mod)
+        {
+            StuffedFloorsCompat.Init();
+
+            MpCompat.harmony.Patch(
+                AccessTools.Method("Clutter_StructureWall.StructureDefGenerator:StuffGeneratrs"),
+                postfix: new HarmonyMethod(typeof(ClutterStructures), nameof(GetClutterStructureDefPostfix))
+                );
+        }
+
+        static void GetClutterStructureDefPostfix(ThingDef Wallzie)
+        {
+            StuffedFloorsCompat.defDatabaseAddMethod.Invoke(null, new object[] { Wallzie });
+        }
+    }
+}
