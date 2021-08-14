a3fa2aae012a02ee78c411e509d35acd2f12b384
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2021-08-14 03:18:44 +0200

    Added compat for Vanilla Social Interactions Expanded (#112)
    This might not be 100% needed, however from my personal testing it was causing issues.

---
+++ Source/Mods/VanillaSocialInteractions.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Vanilla Social Interactions Expanded</summary>
+    /// <see href="https://github.com/AndroidQuazar/VanillaSocialInteractionsExpanded"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2439736083"/>
+    [MpCompatFor("VanillaExpanded.VanillaSocialInteractionsExpanded")]
+    public class VanillaSocialInteractions
+    {
+        public VanillaSocialInteractions(ModContentPack mod) =>
+            PatchingUtilities.PatchPushPopRand("VanillaSocialInteractionsExpanded.SocialInteractionsManager:GameComponentTick");
+    }
+}
