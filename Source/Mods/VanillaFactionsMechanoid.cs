9c1cb16ab0608b6fc42db05340732f745aadcb32
SokyranTheDragon <exploder1531@gmail.com>
2021-08-22 03:28:58 +0200

    Use lambda, fix error spam/potential desync in VFE Mechanoids

--- Source/Mods/VanillaFactionsMechanoid.cs
+++ Source/Mods/VanillaFactionsMechanoid.cs
@@ -1,5 +1,7 @@
-﻿using HarmonyLib;
+﻿using System;
+using HarmonyLib;
 using Multiplayer.API;
+using RimWorld;
 using Verse;
 
 namespace Multiplayer.Compat
@@ -18,22 +20,44 @@ namespace Multiplayer.Compat
             MP.RegisterSyncMethod(type, "StartFire");
             var configureNewTargetMethod = AccessTools.Method(type, "ConfigureNewTarget");
             MP.RegisterSyncMethod(configureNewTargetMethod);
-            foreach (var method in MpCompat.RegisterSyncMethodsByIndex(type, "<GetGizmos>", 2, 3)) method.SetDebugOnly();
+            foreach (var method in MpCompat.RegisterLambdaMethod(type, "GetGizmos", 2, 3)) method.SetDebugOnly();
 
             MpCompat.harmony.Patch(configureNewTargetMethod,
                 postfix: new HarmonyMethod(typeof(VanillaFactionsMechanoid), nameof(CloseWorldTargetter)));
 
             type = AccessTools.TypeByName("VFE.Mechanoids.Buildings.Building_AutoPlant");
-            // Methods b__8_0, b__8_2, b__8_3. They are the first 3 methods in the file. The b__8_1 is the last one, and thus has index 3.
-            MpCompat.RegisterSyncMethodsByIndex(type, "<GetGizmos>", 0, 1, 2);
+            MpCompat.RegisterLambdaMethod(type, "GetGizmos", 0, 2, 3);
+
+            MpCompat.harmony.Patch(AccessTools.Method("VFE.Mechanoids.PlaceWorkers.PlaceWorker_AutoPlant:DrawGhost"),
+                prefix: new HarmonyMethod(typeof(VanillaFactionsMechanoid), nameof(PreDrawGhost)));
         }
 
         private static void CloseWorldTargetter(bool __result)
         {
             // Force close the targetter to give the players a visual cue that it was successful
-            // Otherwise, it would succesfully mark the target but not give any indication
+            // Otherwise, it would successfully mark the target but not give any indication
             if (MP.IsInMultiplayer && __result && Find.WorldTargeter.IsTargeting && Find.WorldTargeter.closeWorldTabWhenFinished)
                 Find.WorldTargeter.StopTargeting();
+        }
+
+        private static void PreDrawGhost(ref Thing thing)
+        {
+            // Fix the bug (which causes error spam) in the original mod if the building is a blueprint or a building frame
+            // This error spam seems capable of causing desyncs
+            switch (thing)
+            {
+                case Blueprint_Build or Frame when thing.def.entityDefToBuild is ThingDef def:
+                    thing = (Thing)Activator.CreateInstance(def.thingClass);
+                    thing.def = def;
+                    break;
+                case Blueprint_Build or Frame:
+                    thing = null;
+                    break;
+                // Handle the case of (re)installing in case another mods lets this be reinstalled
+                case Blueprint_Install install:
+                    thing = install.ThingToInstall;
+                    break;
+            }
         }
     }
 }
