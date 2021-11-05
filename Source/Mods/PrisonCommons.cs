337dcb988ac1a707b6086aa288830d3be41955c6
SokyranTheDragon <exploder1531@gmail.com>
2021-11-05 16:03:32 +0100

    Added Prison Commons compat

---
+++ Source/Mods/PrisonCommons.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Prison Commons by Ben Lubar</summary>
+    /// <see href="https://git.lubar.me/rimworld-mods/prison-commons"/>
+    /// <see href="https://steamcommunity.com/workshop/filedetails/?id=2630896782"/>
+    [MpCompatFor("me.lubar.PrisonCommons")]
+    internal class PrisonCommons
+    {
+        public PrisonCommons(ModContentPack mod) 
+            => LongEventHandler.ExecuteWhenFinished(LatePatch);
+
+        private static void LatePatch()
+            => MpCompat.RegisterLambdaMethod("RimWorld.CompPrisonCommons", "CompGetGizmosExtra", 1);
+    }
+}
