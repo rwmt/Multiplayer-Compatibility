35bc1e7a562912fae4b49c75b27a7db245f81020
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2022-01-17 22:08:27 +0100

    Patched Vanilla Outposts Expanded (#127)

---
+++ Source/Mods/VanillaOutpostsExpanded.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Vanilla Outposts Expanded by legodude17, Oskar Potocki, Chowder, Kikohi</summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2688941031"/>
+    [MpCompatFor("vanillaexpanded.outposts")]
+    internal class VanillaOutpostsExpanded
+    {
+        public VanillaOutpostsExpanded(ModContentPack mod) => LongEventHandler.ExecuteWhenFinished(LatePatch);
+
+        public void LatePatch()
+        {
+            MP.RegisterSyncMethod(AccessTools.TypeByName("VOE.Outpost_Artillery"), "Fire");
+            MpCompat.RegisterLambdaDelegate("VOE.Outpost_Defensive", "GetGizmos", 3);
+        }
+    }
+}
