f8968396eebf0848fc658aae4b92ac2d73303408
SokyranTheDragon <exploder1531@gmail.com>
2022-01-18 19:32:10 +0100

    Compat for Vanilla Ideology Expanded - Memes and Structures

---
+++ Source/Mods/VanillaIdeologyMemes.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Vanilla Ideology Expanded - Memes and Structures by Oskar Potocki, Sarg Bjornson, Chowder</summary>
+    /// <see href="https://github.com/juanosarg/VanillaIdeologyExpanded-Memes"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2636329500"/>
+    [MpCompatFor("VanillaExpanded.VMemesE")]
+    internal class VanillaIdeologyMemes
+    {
+        public VanillaIdeologyMemes(ModContentPack mod)
+        {
+            // RNG
+            {
+                var methods = new[]
+                {
+                    // Commented out the ones that use seeded random, as they should be fine
+                    // If not, then uncommenting those lines should fix it
+                    "VanillaMemesExpanded.CompAbilityHarvestBodyParts:Apply",
+                    //"VanillaMemesExpanded.VanillaMemesExpanded_GameConditionManager_RegisterCondition_Patch:SendRandomMood",
+                    //"VanillaMemesExpanded.VanillaMemesExpanded_GameCondition_Aurora_Init_Patch:SendRandomMood",
+                    //"VanillaMemesExpanded.RitualOutcomeEffectWorker_DivineStars:Apply",
+                    "VanillaMemesExpanded.RitualOutcomeEffectWorker_SlaveEmancipation:Apply",
+                    "VanillaMemesExpanded.RitualOutcomeEffectWorker_ViolentConversion:Apply",
+                };
+
+                PatchingUtilities.PatchSystemRand(methods, false);
+            }
+
+            // Gizmos
+            {
+                var type = AccessTools.TypeByName("VanillaMemesExpanded.CompDeconstructJunk");
+
+                MP.RegisterSyncMethod(type, "SetObjectForDeconstruction");
+                MP.RegisterSyncMethod(type, "CancelObjectForDeconstruction");
+            }
+        }
+    }
+}
