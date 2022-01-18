b297ce1d47003015a204fe37a72d24bf64b4b5aa
SokyranTheDragon <exploder1531@gmail.com>
2022-01-18 02:05:04 +0100

    Patched Vanilla Ideology Expanded - Dryads

---
+++ Source/Mods/VanillaIdeologyDryads.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Multiplayer.API;
+using Multiplayer.Compat;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Vanilla Ideology Expanded - Dryads by Oskar Potocki, Sarg Bjornson, Taranchuk, Reann Shepard</summary>
+    /// <see href="https://github.com/juanosarg/VanillaIdeologyExpanded-Dryads"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2720631512"/>
+    [MpCompatFor("VanillaExpanded.Ideo.Dryads")]
+    internal class VanillaIdeologyDryads
+    {
+        public VanillaIdeologyDryads(ModContentPack mod)
+        {
+            // RNG
+            PatchingUtilities.PatchSystemRandCtor("VanillaIdeologyExpanded_Dryads.HediffComp_PeriodicWounds");
+
+            // Gizmos
+            MP.RegisterSyncMethod(AccessTools.TypeByName("VanillaIdeologyExpanded_Dryads.CompPawnMerge"), "SetDryadAwakenPod");
+            MpCompat.RegisterLambdaMethod("VanillaIdeologyExpanded_Dryads.CompSpawnAwakened", "CompGetGizmosExtra", 0).SetDebugOnly();
+        }
+    }
+}
