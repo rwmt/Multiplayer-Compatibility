0fed46746c7350445985cf6c44a7b7ce7f34426d
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2022-01-20 01:38:09 +0100

    Compat for Alpha Memes (#131)

---
+++ Source/Mods/AlphaMemes.cs
@@ -0,0 +1,20 @@
+﻿using Multiplayer.Compat;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Alpha Memes by Sarg Bjornson, Helixien, Cassie, Luizi</summary>
+    /// <see href="https://github.com/juanosarg/AlphaMemes"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2661356814"/>
+    [MpCompatFor("Sarg.AlphaMemes")]
+    internal class AlphaMemes
+    {
+        public AlphaMemes(ModContentPack mod)
+        {
+            PatchingUtilities.PatchSystemRand("AlphaMemes.AlphaMemes_DamageWorker_AddInjury_Apply_Patch:SendHistoryIfMelee", false);
+            // The following method is seeded, so it should be fine
+            // If not, then patching it as well should fix it
+            //"AlphaMemes.GameComponent_RandomMood:GameComponentTick",
+        }
+    }
+}
