9fd45000970b33f1c32ae600dfd4d2f579409d6e
SokyranTheDragon <exploder1531@gmail.com>
2022-01-20 02:17:46 +0100

    Synced turning on/off curses

--- Source/Mods/VanillaFactionsPirates.cs
+++ Source/Mods/VanillaFactionsPirates.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
 using Multiplayer.API;
 using UnityEngine;
@@ -8,19 +11,33 @@ using Verse;
 
 namespace Multiplayer.Compat
 {
-    /// <summary></summary>
-    /// <see href=""/>
+    /// <summary>Vanilla Factions Expanded - Pirates by Oskar Potocki, Sarg Bjornson, erdelf, Roolo, Smash Phil, Taranchuk, xrushha, Kikohi, legodude17</summary>
+    /// <see href="https://github.com/AndroidQuazar/VanillaFactionsExpanded-Pirates"/>
     /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2723801948"/>
     [MpCompatFor("OskarPotocki.VFE.Pirates")]
     internal class VanillaFactionsPirates
     {
+        // Warcasket
+        // Dialog_WarcasketCustomization
         private static Type warcasketDialogType;
         private static MethodInfo notifySettingsChangedMethod;
         private static FieldInfo warcasketProjectField;
-
+        // WarcasketProject
         private static FieldInfo helmetColorField;
         private static FieldInfo shoulderPadsColorField;
         private static FieldInfo armorColorField;
+
+        // Curses
+        // GameComponent_CurseManager
+        private static FieldInfo curseManagerInstanceField;
+        private static FieldInfo activeCurseDefsField;
+        private static MethodInfo removeCurseMethod;
+        private static MethodInfo addCurseMethod;
+        // CurseDef
+        private static MethodInfo curseDefWorkerGetter;
+        // CurseWorker
+        private static MethodInfo curseWorkerDisactivateMethod;
+        private static MethodInfo curseWorkerStartMethod;
 
         public VanillaFactionsPirates(ModContentPack mod)
         {
@@ -34,6 +51,7 @@ namespace Multiplayer.Compat
 
             // Warcasket dialog
             {
+                // Dialog_WarcasketCustomization
                 warcasketDialogType = AccessTools.TypeByName("VFEPirates.Buildings.Dialog_WarcasketCustomization");
                 notifySettingsChangedMethod = AccessTools.Method(warcasketDialogType, "Notify_SettingsChanged");
                 warcasketProjectField = AccessTools.Field(warcasketDialogType, "project");
@@ -48,7 +66,8 @@ namespace Multiplayer.Compat
                 MpCompat.harmony.Patch(AccessTools.Method(warcasketDialogType, "DoWindowContents"),
                     prefix: new HarmonyMethod(typeof(VanillaFactionsPirates), nameof(PreDoWindowContents)),
                     postfix: new HarmonyMethod(typeof(VanillaFactionsPirates), nameof(PostDoWindowContents)));
-                
+
+                // WarcasketProject
                 var type = AccessTools.TypeByName("VFEPirates.WarcasketProject");
 
                 helmetColorField = AccessTools.Field(type, "colorHelmet");
@@ -56,6 +75,31 @@ namespace Multiplayer.Compat
                 armorColorField = AccessTools.Field(type, "colorArmor");
 
                 MP.RegisterSyncMethod(typeof(VanillaFactionsPirates), nameof(SyncedSetColors));
+            }
+
+            // Curse window
+            {
+                // Page_ChooseCurses
+                MP.RegisterSyncMethod(typeof(VanillaFactionsPirates), nameof(SyncedSetCurse));
+                MpCompat.harmony.Patch(AccessTools.Method("VFEPirates.Page_ChooseCurses:DoCurse"),
+                    transpiler: new HarmonyMethod(typeof(VanillaFactionsPirates), nameof(ReplaceButton)));
+
+                // GameComponent_CurseManager
+                var type = AccessTools.TypeByName("VFEPirates.GameComponent_CurseManager");
+
+                curseManagerInstanceField = AccessTools.Field(type, "Instance");
+                activeCurseDefsField = AccessTools.Field(type, "activeCurseDefs");
+                removeCurseMethod = AccessTools.Method(type, "Remove");
+                addCurseMethod = AccessTools.Method(type, "Add");
+
+                // CurseDef
+                curseDefWorkerGetter = AccessTools.PropertyGetter(AccessTools.TypeByName("VFEPirates.CurseDef"), "Worker");
+
+                // CurseWorker
+                type = AccessTools.TypeByName("VFEPirates.CurseWorker");
+
+                curseWorkerDisactivateMethod = AccessTools.Method(type, "Disactivate");
+                curseWorkerStartMethod = AccessTools.Method(type, "Start");
             }
         }
 
@@ -80,8 +124,8 @@ namespace Multiplayer.Compat
 
             var project = warcasketProjectField.GetValue(__instance);
 
-            var helmetColor = (Color) helmetColorField.GetValue(project);
-            var shoulderPadsColor = (Color) shoulderPadsColorField.GetValue(project);
+            var helmetColor = (Color)helmetColorField.GetValue(project);
+            var shoulderPadsColor = (Color)shoulderPadsColorField.GetValue(project);
             var armorColor = (Color)armorColorField.GetValue(project);
 
             if (__state[0] != helmetColor || __state[1] != shoulderPadsColor || __state[2] != armorColor)
@@ -117,6 +161,67 @@ namespace Multiplayer.Compat
         {
             if (!sync.isWriting)
                 dialog = Find.WindowStack.Windows.FirstOrDefault(w => w.GetType() == warcasketDialogType);
+        }
+
+        private static void SyncedSetCurse(Def curseDef)
+        {
+            var curseManager = curseManagerInstanceField.GetValue(null);
+            var set = activeCurseDefsField.GetValue(curseManager) as IEnumerable;
+            var curseWorker = curseDefWorkerGetter.Invoke(curseDef, Array.Empty<object>());
+
+            var found = false;
+
+            foreach (var def in set)
+            {
+                if (def == curseDef)
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found)
+            {
+                removeCurseMethod.Invoke(curseManager, new object[] { curseDef });
+                curseWorkerDisactivateMethod.Invoke(curseWorker, Array.Empty<object>());
+            }
+            else
+            {
+                addCurseMethod.Invoke(curseManager, new object[] { curseDef });
+                curseWorkerStartMethod.Invoke(curseWorker, Array.Empty<object>());
+            }
+        }
+
+        private static bool InjectedButton(Rect rect, bool doMouseoverSound, Def curseDef)
+        {
+            if (Widgets.ButtonInvisible(rect, doMouseoverSound))
+            {
+                if (MP.IsInMultiplayer)
+                    SyncedSetCurse(curseDef);
+                // Not in MP - return true as the button was clicked
+                else 
+                    return true;
+            }
+
+            return false;
+        }
+
+        private static IEnumerable<CodeInstruction> ReplaceButton(IEnumerable<CodeInstruction> instr)
+        {
+            var method = AccessTools.Method(typeof(Widgets), nameof(Widgets.ButtonInvisible));
+
+            foreach (var ci in instr)
+            {
+                if (ci.opcode == OpCodes.Call && ci.operand is MethodInfo info && info == method)
+                {
+                    // Inject another argument (CurseDef)
+                    yield return new CodeInstruction(OpCodes.Ldarg_2);
+
+                    ci.operand = AccessTools.Method(typeof(VanillaFactionsPirates), nameof(InjectedButton));
+                }
+
+                yield return ci;
+            }
         }
     }
 }
