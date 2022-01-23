c282a6d660e6e4efb52c85ad9ea02d490545f019
SokyranTheDragon <exploder1531@gmail.com>
2022-01-23 22:54:09 +0100

    Added compat for Toggleable Shields

---
+++ Source/Mods/ToggleableShields.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Toggleable Shields by Owlchemist</summary>
+    /// <see href="https://github.com/Owlchemist/toggleable-shields"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2690413766"/>
+    [MpCompatFor("Owlchemist.ToggleableShields")]
+    internal class ToggleableShields
+    {
+        public ToggleableShields(ModContentPack mod) 
+            => MpCompat.RegisterLambdaDelegate("ToggleableShields.Patch_GetWornGizmos", "Postfix", 1);
+    }
+}
