0275c4953c2a876a4c5edce3bac0bdbcaddc7e02
SokyranTheDragon <exploder1531@gmail.com>
2022-02-07 12:45:36 +0100

    Compat for Autoharvester Auto-cycle

---
+++ Source/Mods/AutoharvesterAutocycle.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Autoharvester Auto-cycle by FluffyKittens</summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2603143540"/>
+    [MpCompatFor("FluffyKittens.Autoharvester")]
+    internal class AutoharvesterAutocycle
+    {
+        public AutoharvesterAutocycle(ModContentPack mod) 
+            => MpCompat.RegisterLambdaMethod("AutoHarvesterCycle.CycleComp", "CompGetGizmosExtra", 0, 1, 2, 3);
+    }
+}
