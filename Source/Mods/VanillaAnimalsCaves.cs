b2ed1a61f4b0bb9ace495a59b21e7e1b5f95f553
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2022-02-09 16:42:34 +0100

    Added compat for Vanilla Animals Expanded — Caves (#169)

---
+++ Source/Mods/VanillaAnimalsCaves.cs
@@ -0,0 +1,34 @@
+﻿using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Vanilla Animals Expanded — Caves by Oskar Potocki, Sarg Bjornson, Aquiles</summary>
+    /// <see href="https://github.com/juanosarg/VanillaAnimalsExpanded-Caves"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2576512001"/>
+    [MpCompatFor("VanillaExpanded.VAECaves")]
+    internal class VanillaAnimalsCaves
+    {
+        public VanillaAnimalsCaves(ModContentPack mod)
+        {
+            // RNG
+            {
+                var methods = new[]
+                {
+                    "VAECaves.Building_SpiderEggs:Tick",
+                    "VAECaves.DamageWorker_TwoArmSlam:Apply",
+                    "VAECaves.IncidentWorker_Hulk:CanFireNowSub",
+                    "VAECaves.JobDriver_CreateEggs:<MakeNewToils>b__1_0",
+                };
+
+                PatchingUtilities.PatchSystemRand(methods, false);
+                PatchingUtilities.PatchSystemRandCtor("VAECaves.Hediff_WallBreaker", false);
+            }
+
+            // Gizmos
+            {
+                MpCompat.RegisterLambdaMethod("VAECaves.Building_Cocoon", "GetGizmos", 0);
+                MpCompat.RegisterLambdaMethod("VAECaves.CompConditionalSpawner", "CompGetGizmosExtra", 0).SetDebugOnly();
+            }
+        }
+    }
+}
