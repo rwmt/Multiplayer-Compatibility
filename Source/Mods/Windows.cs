444facaa452a66e94c816ecb7f9182b34c7563b5
SokyranTheDragon <exploder1531@gmail.com>
2022-02-18 20:37:08 +0100

    Added compatibility for Windows

---
+++ Source/Mods/Windows.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Windows by Owlchemist, jptrrs</summary>
+    /// <see href="https://github.com/Owlchemist/OpenTheWindows"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2571189146"/>
+    [MpCompatFor("Owlchemist.Windows")]
+    internal class Windows
+    {
+        public Windows(ModContentPack mod)
+        {
+            var type = AccessTools.TypeByName("OpenTheWindows.Building_Window");
+            MpCompat.RegisterLambdaMethod(type, "GetGizmos", 1, 3);
+
+            type = AccessTools.TypeByName("OpenTheWindows.CompWindow");
+            MpCompat.RegisterLambdaMethod(type, "CompGetGizmosExtra", 1);
+        }
+    }
+}
