a11086523e655825716add8aa8617ebe499e1761
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2022-07-02 05:51:24 +0200

    Fixes #194, adds a PauseLock for power choice gizmo (#207)
    Co-authored-by: Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>

--- Source/Mods/VanillaFactionsAncients.cs
+++ Source/Mods/VanillaFactionsAncients.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
 using Multiplayer.API;
+using UnityEngine;
 using Verse;
 
 namespace Multiplayer.Compat
@@ -11,7 +17,13 @@ namespace Multiplayer.Compat
     [MpCompatFor("VanillaExpanded.VFEA")]
     internal class VanillaFactionsAncients
     {
+        private delegate void CallOnChosen(Window dialog, Def power, Def weakness);
+
         private static AccessTools.FieldRef<object, ThingComp> operationPodField;
+        private static CallOnChosen onChosen;
+        private static Type choosePowerDialogType;
+
+        private static Window currentDialog = null;
 
         public VanillaFactionsAncients(ModContentPack mod)
         {
@@ -23,6 +35,8 @@ namespace Multiplayer.Compat
             var type = AccessTools.TypeByName("VFEAncients.Operation");
             operationPodField = AccessTools.FieldRefAccess<ThingComp>(type, "Pod");
             MP.RegisterSyncWorker<object>(SyncOperation, type, true);
+
+            MP.RegisterPauseLock(ShouldPause);
 
             LongEventHandler.ExecuteWhenFinished(LatePatch);
         }
@@ -43,7 +57,20 @@ namespace Multiplayer.Compat
             // (Dev) instant finish, random result not synced, as it calls CompleteOperation
             // would cause a tiny conflict, not worth bothering with it
             // (I think it would need to be done without SetDebugOnly, or it would cause issues)
+
+            choosePowerDialogType = AccessTools.TypeByName("VFEAncients.Dialog_ChoosePowers");
+            var powerDefType = AccessTools.TypeByName("VFEAncients.PowerDef");
+            var tupleType = typeof(Tuple<,>).MakeGenericType(powerDefType, powerDefType);
+            onChosen = CompileCallOnChosen(powerDefType, tupleType);
+            MP.RegisterSyncMethod(typeof(VanillaFactionsAncients), nameof(SyncedChoosePower));
+            MP.RegisterSyncWorker<Window>(SyncDialogChoosePower, choosePowerDialogType);
+            MpCompat.harmony.Patch(AccessTools.DeclaredConstructor(choosePowerDialogType, new[] { typeof(List<>).MakeGenericType(tupleType), typeof(Pawn), typeof(Action<>).MakeGenericType(tupleType) }),
+                postfix: new HarmonyMethod(typeof(VanillaFactionsAncients), nameof(PostDialogConstructor)));
+            MpCompat.harmony.Patch(AccessTools.Method(choosePowerDialogType, nameof(Window.DoWindowContents)),
+                transpiler: new HarmonyMethod(typeof(VanillaFactionsAncients), nameof(ReplaceButtons)));
         }
+
+        #region SyncWorkers
 
         private static void SyncOperation(SyncWorker sync, ref object operation)
         {
@@ -62,5 +89,97 @@ namespace Multiplayer.Compat
                 operation = Activator.CreateInstance(type, pod);
             }
         }
+
+        private static void SyncDialogChoosePower(SyncWorker sync, ref Window window)
+        {
+            if (!sync.isWriting)
+                window = currentDialog ?? Find.WindowStack.windows.FirstOrDefault(x => x.GetType() == choosePowerDialogType);
+        }
+
+        #endregion
+
+        #region PauseLock
+
+        private static bool ShouldPause(Map map)
+        {
+            if (currentDialog == null) return false;
+            if (currentDialog.IsOpen) return true;
+
+            currentDialog = null;
+            return false;
+        }
+
+        #endregion
+
+        #region Dialog
+
+        private static void PostDialogConstructor(Window __instance) => currentDialog = __instance;
+
+        private static bool ChoosePower(Rect rect, string text, bool drawBackground, bool doMouseoverSound, bool active, Def power, Def weakness)
+        {
+            var buttonResult = Widgets.ButtonText(rect, text, drawBackground, doMouseoverSound, active);
+
+            if (!MP.IsInMultiplayer || !buttonResult)
+                return buttonResult;
+
+            SyncedChoosePower(power, weakness);
+            return false;
+        }
+
+        private static void SyncedChoosePower(Def power, Def weakness)
+        {
+            var dialog = currentDialog ?? Find.WindowStack.windows.FirstOrDefault(x => x.GetType() == choosePowerDialogType);
+            if (dialog == null) return;
+
+            onChosen(dialog, power, weakness);
+            dialog.Close();
+        }
+
+        private static IEnumerable<CodeInstruction> ReplaceButtons(IEnumerable<CodeInstruction> instr)
+        {
+            var target = AccessTools.Method(typeof(Widgets), nameof(Widgets.ButtonText), new[] { typeof(Rect), typeof(string), typeof(bool), typeof(bool), typeof(bool) });
+            var replacement = AccessTools.Method(typeof(VanillaFactionsAncients), nameof(ChoosePower));
+
+            foreach (var ci in instr)
+            {
+                if (ci.opcode == OpCodes.Call && ci.operand is MethodInfo method && method == target)
+                {
+                    ci.operand = replacement;
+
+                    yield return new CodeInstruction(OpCodes.Ldloc_1); // Power
+                    yield return new CodeInstruction(OpCodes.Ldloc_2); // Weakness
+                }
+
+                yield return ci;
+            }
+        }
+
+        private static CallOnChosen CompileCallOnChosen(Type powerDefType, Type tupleType)
+        {
+            // Setup the parameters to the delegate
+            var dialogParam = Expression.Parameter(typeof(Window), "dialog");
+            var powerParam = Expression.Parameter(typeof(Def), "power");
+            var weaknessParam = Expression.Parameter(typeof(Def), "weakness");
+
+            // We'll need to cast from Window to Dialog_ChoosePower and Def to PowerDef to be able to construct our Tuple<PowerDef,PowerDef>
+            var dialogCast = Expression.Convert(dialogParam, choosePowerDialogType);
+            var powerCast = Expression.Convert(powerParam, powerDefType);
+            var weaknessCast = Expression.Convert(weaknessParam, powerDefType);
+
+            // Create the tuple from our parameters after casting them
+            var newTuple = Expression.New(AccessTools.Constructor(tupleType, new[] { powerDefType, powerDefType }), powerCast, weaknessCast);
+
+            // Get the field onChosen of type Action<PowerDef,PowerDef> from Dialog_ChoosePower
+            var onChosenField = Expression.Field(dialogCast, AccessTools.Field(choosePowerDialogType, "onChosen"));
+            // Invoke the action with the tuple as our parameter
+            var invoke = Expression.Invoke(onChosenField, newTuple);
+
+            // Create the expression lambda that'll do all our operations
+            var lambda = Expression.Lambda(typeof(CallOnChosen), invoke, dialogParam, powerParam, weaknessParam);
+            // Compile and return the lambda
+            return (CallOnChosen)lambda.Compile();
+        }
+
+        #endregion
     }
 }
