deef7673f3d7e5173dcabb470888ebc6f882d560
Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>
2022-07-02 21:54:13 +0200

    Slightly changed how loading .dll for referenced mods works
    - Added MpCompatRequireModAttribute, which works similar to MpCompatForAttribute in that it'll be removed from loaded assembly if the mod is not active. The main difference is that it won't be loaded as a compat to the mod.
    - MpCompatLoader looks for all MpCompatForAttribute and MpCompatRequireModAttribute attributes (instead of a single one)
    - It'll check for each mod referenced by those attributes. If none are found, it'll remove the type from assembly.
    - The assembly is added to loaded assemblies. Removed the call to InitCompatInAsm, as it's no longer needed because of that change.
    - Cleared all types from Multiplayer cache for GenTypes and regenerated it again. It's needed as GenTypes AllSubclasses/AllSubclassesNonAbstract would not be able to find classes from referenced .dll file. Needed to make the create GameComponents, and similar stuff, that we define in referenced .dll.

---
+++ Source/MpCompatRequireModAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Multiplayer.Compat
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class MpCompatRequireModAttribute : Attribute
+    {
+        public string PackageId { get; }
+
+        public MpCompatRequireModAttribute(string packageId) => PackageId = packageId;
+
+        public override object TypeId => this;
+    }
+}
