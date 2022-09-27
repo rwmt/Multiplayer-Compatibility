b916f5cc6433f8665121c7581c0928838d0606d3
Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>
2022-09-27 19:40:49 +0200

    Added compat for Rim-Effect mods
    This should handle everything in the Rim-Effect mods that needs patching, with 1 exception - quest menu from galaxy map.
    
    That feature requires patching Vanilla Expanded Framework, and is bit more complex than the rest of the patches. Leaving it for later.

---
+++ Source/Mods/RimEffectDrell.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Rim-Effect: Drell by Vanilla Expanded Team and Co.</summary>
+    /// <see href="https://github.com/AndroidQuazar/RimEffect-Drell"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2651150217"/>
+    [MpCompatFor("RimEffect.Drell")]
+    public class RimEffectDrell
+    {
+        public RimEffectDrell(ModContentPack mod) 
+            => MpCompat.RegisterLambdaMethod("REDrell.Comp_Drell", "CompGetGizmosExtra", 0, 1);
+    }
+}
