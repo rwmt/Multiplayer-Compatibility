0e3aa573ffdaaf543e4a5aa06de76191417532e0
Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>
2022-09-29 00:45:34 +0200

    Fix sauna desyncs in Dubs Bad Hygiene

---
+++ Source/Mods/BadHygiene.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Dubs Bad Hygiene by Dubwise</summary>
+    /// <see href="https://github.com/Dubwise56/Dubs-Bad-Hygiene"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=836308268"/>
+    [MpCompatFor("Dubwise.DubsBadHygiene")]
+    public class BadHygiene
+    {
+        public BadHygiene(ModContentPack mod)
+        {
+            var type = AccessTools.TypeByName("DubsBadHygiene.Comp_SaunaHeater");
+            var methods = new[]
+            {
+                AccessTools.Method(type, "SteamyNow"),
+                AccessTools.Method(type, "CompTick"),
+            };
+
+            PatchingUtilities.PatchPushPopRand(methods);
+        }
+    }
+}
