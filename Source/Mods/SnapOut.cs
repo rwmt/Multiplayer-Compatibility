5fb1706efc4eb2e72cbd981f714398c15a77eb32
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2022-09-30 22:04:26 +0200

    Fixed RNG related desync in Snap Out (#227)
    Co-authored-by: Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>

---
+++ Source/Mods/SnapOut.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Snap Out! by Weilbyte</summary>
+    /// <see href="https://github.com/Weilbyte/SnapOut"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1319782555"/>
+    [MpCompatFor("weilbyte.snapout")]
+    public class SnapOut
+    {
+        public SnapOut(ModContentPack mod)
+        {
+            var type = AccessTools.TypeByName("SnapOut.JobDriver_GoToSafety");
+            type = AccessTools.FirstInner(type, _ => true); // There should be only 1 inner class anyway
+            PatchingUtilities.PatchUnityRand(AccessTools.Method(type, "MoveNext"), false);
+        }
+    }
+}
