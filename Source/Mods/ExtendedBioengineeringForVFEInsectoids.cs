14a9d09f94219575ed0ba8f21d5dd5498bd69be5
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2022-10-02 03:53:46 +0200

    Added compat for Extended Bioengineering for VFE Insectoids (#230)
    Co-authored-by: Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>

---
+++ Source/Mods/ExtendedBioengineeringForVFEInsectoids.cs
@@ -0,0 +1,50 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Extended Bioengineering for VFE Insectoids by Turnovus</summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2706534548"/>
+    [MpCompatFor("turnovus.submod.extendedbioengineering")]
+    public class ExtendedBioengineeringForVFEInsectoids
+    {
+        public ExtendedBioengineeringForVFEInsectoids(ModContentPack mod)
+        {
+            // Gizmos
+            {
+                var type = AccessTools.TypeByName("extendedbioengineering.Building_Inducer");
+                MP.RegisterSyncMethod(type, "TryFireInducerNow");
+                MP.RegisterSyncMethod(type, "SetCountdownToNearZero").SetDebugOnly();
+
+                // Unused - artificial hive is not included in the mod
+                // MP.RegisterSyncMethod(AccessTools.Method("extendedbioengineering.Building_ArtificialHive:TryRequestNewCulture"));
+            }
+
+            // RNG
+            {
+                var type = AccessTools.TypeByName("extendedbioengineering.OutputWorker_Dissect");
+                type = AccessTools.FirstInner(type, _ => true); // There should be only 1 inner class
+                PatchingUtilities.PatchSystemRand(AccessTools.Method(type, "MoveNext"), false);
+
+                type = AccessTools.TypeByName("extendedbioengineering.OutputWorker_Genes");
+                type = AccessTools.FirstInner(type, _ => true); // There should be only 1 inner class
+                PatchingUtilities.PatchSystemRand(AccessTools.Method(type, "MoveNext"), false);
+            }
+
+            LongEventHandler.ExecuteWhenFinished(LatePatch);
+        }
+
+        private static void LatePatch()
+        {
+            // Gizmos
+            {
+                var type = AccessTools.TypeByName("extendedbioengineering.ThingComp_JellySynthesizer");
+
+                MP.RegisterSyncMethod(type, "TrySetNewSetting");
+                MP.RegisterSyncMethod(AccessTools.PropertySetter(type, "TargetFuel"));
+                MpCompat.RegisterLambdaMethod(type, "CompGetGizmosExtra", 1);
+            }
+        }
+    }
+}
