b9c252717931b8ff051dbb41ae23d2df5bc45c8d
Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>
2022-11-06 20:16:33 +0100

    Fixed Bad Hygiene designator syncing

--- Source/Mods/BadHygiene.cs
+++ Source/Mods/BadHygiene.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using Multiplayer.API;
 using Verse;
 
 namespace Multiplayer.Compat
@@ -9,16 +10,49 @@ namespace Multiplayer.Compat
     [MpCompatFor("Dubwise.DubsBadHygiene")]
     public class BadHygiene
     {
+        private static AccessTools.FieldRef<Designator, ThingDef> removePlumbingRemovalModeField;
+        private static AccessTools.FieldRef<Designator, bool> placeFertilizerAddingField;
+
         public BadHygiene(ModContentPack mod)
         {
-            var type = AccessTools.TypeByName("DubsBadHygiene.Comp_SaunaHeater");
-            var methods = new[]
+            // RNG
             {
-                AccessTools.Method(type, "SteamyNow"),
-                AccessTools.Method(type, "CompTick"),
-            };
+                var type = AccessTools.TypeByName("DubsBadHygiene.Comp_SaunaHeater");
+                var methods = new[]
+                {
+                    AccessTools.Method(type, "SteamyNow"),
+                    AccessTools.Method(type, "CompTick"),
+                };
 
-            PatchingUtilities.PatchPushPopRand(methods);
+                PatchingUtilities.PatchPushPopRand(methods);
+            }
+
+            // Designators
+            {
+                var type = AccessTools.TypeByName("DubsBadHygiene.Designator_AreaPlaceFertilizer");
+                placeFertilizerAddingField = AccessTools.FieldRefAccess<bool>(type, "Adding");
+                MP.RegisterSyncWorker<Designator>(SyncFertilizerAreaDesignator, type, shouldConstruct: true);
+
+                type = AccessTools.TypeByName("DubsBadHygiene.Designator_RemovePlumbing");
+                removePlumbingRemovalModeField = AccessTools.FieldRefAccess<ThingDef>(type, "RemovalMode");
+                MP.RegisterSyncWorker<Designator>(SyncRemovePlumbingDesignator, type, shouldConstruct: true);
+            }
+        }
+
+        private static void SyncRemovePlumbingDesignator(SyncWorker sync, ref Designator designator)
+        {
+            if (sync.isWriting)
+                sync.Write(removePlumbingRemovalModeField(designator));
+            else
+                removePlumbingRemovalModeField(designator) = sync.Read<ThingDef>();
+        }
+
+        private static void SyncFertilizerAreaDesignator(SyncWorker sync, ref Designator designator)
+        {
+            if (sync.isWriting)
+                sync.Write(placeFertilizerAddingField(designator));
+            else
+                placeFertilizerAddingField(designator) = sync.Read<bool>();
         }
     }
 }
