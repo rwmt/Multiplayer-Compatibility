1f1070014dd9a4036b8c3528bf1e5fcf12f83b1c
Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>
2022-11-06 20:20:57 +0100

    Added compat for Grazing Lands

---
+++ Source/Mods/GrazingLands.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Grazing Lands by avilmask</summary>
+    [MpCompatFor("avilmask.GrazingLands")]
+    public class GrazingLands
+    {
+        public GrazingLands(ModContentPack mod)
+        {
+            var type = AccessTools.TypeByName("GrazingLands.PlantPropertiesPatch");
+            type = AccessTools.Inner(type, "Plant_IngestedCalculateAmounts_GrazingLandsPatch");
+            PatchingUtilities.PatchSystemRand(AccessTools.Method(type, "Prefix"), false);
+        }
+    }
+}
