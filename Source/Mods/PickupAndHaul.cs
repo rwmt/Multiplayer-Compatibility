44ce8e612cb2cf24183faa198d901ea44fce0f19
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2022-11-11 17:40:13 +0100

    Added compat for Pickup and Haul (#246)
    Co-authored-by: Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>

---
+++ Source/Mods/PickupAndHaul.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Pick Up And Haul by Mehni</summary>
+    /// <see href="https://github.com/Mehni/PickUpAndHaul/"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1279012058"/>
+    [MpCompatFor("Mehni.PickUpAndHaul")]
+    public class PickupAndHaul
+    {
+        public PickupAndHaul(ModContentPack mod)
+        {
+            // Sorts the ListerHaulables list from UI, causes issues
+            MpCompat.harmony.Patch(AccessTools.Method("PickUpAndHaul.WorkGiver_HaulToInventory:PotentialWorkThingsGlobal"),
+                transpiler: new HarmonyMethod(typeof(PickupAndHaul), nameof(Transpiler)));
+        }
+
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
+        {
+            var target = AccessTools.Method(typeof(ListerHaulables), nameof(ListerHaulables.ThingsPotentiallyNeedingHauling));
+            var newListCtor = AccessTools.Constructor(typeof(List<Thing>), new[] { typeof(IEnumerable<Thing>) });
+
+            var patched = false;
+            foreach (var ci in instr)
+            {
+                yield return ci;
+
+                if (ci.opcode == OpCodes.Callvirt && ci.operand is MethodInfo method && method == target)
+                {
+                    yield return new CodeInstruction(OpCodes.Newobj, newListCtor);
+                    patched = true;
+                }
+            }
+
+            if (!patched)
+                throw new Exception("Failed patching Pickup and Haul");
+        }
+    }
+}
