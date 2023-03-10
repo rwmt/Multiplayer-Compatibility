2a3ac8f6f92e3a8a8dc62289b7a0f66162b49519
Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>
2023-03-10 22:35:28 +0100

    Check in MP if sync worker for given type exists

--- Source/MpSyncWorkers.cs
+++ Source/MpSyncWorkers.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
+using HarmonyLib;
 using Multiplayer.API;
 using RimWorld;
 using Verse;
@@ -9,25 +9,20 @@ namespace Multiplayer.Compat
 {
     public static class MpSyncWorkers
     {
-        private static HashSet<Type> registeredSyncWorkers = null;
-
         public static void Requires<T>() => Requires(typeof(T));
 
         public static void Requires(Type type)
         {
-            if (registeredSyncWorkers != null && registeredSyncWorkers.Contains(type))
+            if (HasSyncWorker(type))
+            {
+                Log.Warning($"Sync worker of type {type} already exists in MP, temporary sync worker can be removed from MP Compat");
                 return;
+            }
 
             if (type == typeof(IGeneResourceDrain))
                 MP.RegisterSyncWorker<IGeneResourceDrain>(SyncIGeneResourceDrain);
             else
-            {
                 Log.Error($"Trying to register SyncWorker of type {type}, but it's not supported.\n{new StackTrace(1)}");
-                return;
-            }
-
-            registeredSyncWorkers ??= new HashSet<Type>();
-            registeredSyncWorkers.Add(type);
         }
 
         private static void SyncIGeneResourceDrain(SyncWorker sync, ref IGeneResourceDrain resourceDrain)
@@ -41,6 +36,28 @@ namespace Multiplayer.Compat
             }
             else
                 resourceDrain = sync.Read<Gene>() as IGeneResourceDrain;
+        }
+
+        private static bool HasSyncWorker(Type type)
+        {
+            const string methodPath = "Multiplayer.Client.SyncSerialization:CanHandle";
+
+            // Don't cache the method, it'll be used very rarely (assuming it'll even be used at all), so there's no point in having a field for it.
+            var method = AccessTools.DeclaredMethod(methodPath, new[] { typeof(SyncType) });
+
+            if (method == null)
+            {
+                Log.Error($"Failed to check if sync worker for type {type} is already registered in MP - failed to find method {methodPath}");
+                return false;
+            }
+
+            if (method.ReturnType != typeof(bool))
+            {
+                Log.Error($"Failed to check if sync worker for type {type} is already registered in MP - return type is not bool but {method.ReturnType} for method {methodPath}");
+                return false;
+            }
+
+            return (bool)method.Invoke(null, new object[] { new SyncType(type) });
         }
     }
 }
