0359ed76039ef3200c54b246ddf230d75dddccdd
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2023-03-10 01:10:35 +0100

    Added compat for Biotech Expansion - Mythic (#275)
    Co-authored-by: Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>

---
+++ Source/Mods/BiotechExpansionMythic.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using HarmonyLib;
+using Multiplayer.API;
+using RimWorld;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Biotech Expansion - Mythic by Lennoxicon</summary>
+    /// <see href="https://github.com/Lennoxite/bte-mythic"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2883216840"/>
+    [MpCompatFor("biotexpans.mythic")]
+    public class BiotechExpansionMythic
+    {
+        private static Type geneAurumType;
+        private static Type geneReverenceType;
+
+        private static ISyncField syncAurumAllowedField;
+        private static ISyncField syncReverenceAllowedField;
+
+        public BiotechExpansionMythic(ModContentPack mod)
+        {
+            TemporarySyncWorkers.RegisterIGeneResourceDrain();
+
+            var type = AccessTools.TypeByName("BTE_MY.CompCreateReveredMote");
+            MP.RegisterSyncMethod(type, "GetMeditationSpots"); // Scan for meditation spots
+            MP.RegisterSyncMethod(type, "AddProgress").SetDebugOnly(); // (Dev) add 100% progress (also called during ticking)
+
+            // -/+ 10% resource
+            MP.RegisterSyncMethod(AccessTools.DeclaredMethod("BTE_MY.AurumUtility:OffsetResource")).SetDebugOnly();
+            MP.RegisterSyncMethod(AccessTools.DeclaredMethod("BTE_MY.ReverenceUtility:OffsetResource")).SetDebugOnly();
+
+            geneAurumType = type = AccessTools.TypeByName("BTE_MY.Gene_Aurum");
+            syncAurumAllowedField = MP.RegisterSyncField(type, "aurumFuelAllowed");
+
+            geneReverenceType = type = AccessTools.TypeByName("BTE_MY.Gene_Reverence");
+            syncReverenceAllowedField = MP.RegisterSyncField(type, "ReverenceFuelAllowed");
+
+            LongEventHandler.ExecuteWhenFinished(LatePatch);
+        }
+
+        private static void LatePatch()
+        {
+            foreach (var typeName in new[] { "BTE_MY.GeneGizmo_ResourceAurum", "BTE_MY.GeneGizmo_ResourceReverence" })
+            {
+                MpCompat.harmony.Patch(AccessTools.DeclaredMethod($"{typeName}:{nameof(GeneGizmo_Resource.DrawLabel)}"),
+                    prefix: new HarmonyMethod(typeof(BiotechExpansionMythic), nameof(PreDrawLabel)));
+            }
+        }
+
+        private static void PreDrawLabel(GeneGizmo_Resource __instance)
+        {
+            if (!MP.IsInMultiplayer)
+                return;
+
+            var type = __instance.gene.GetType();
+            if (type == geneAurumType)
+                syncAurumAllowedField.Watch(__instance.gene);
+            else if (type == geneReverenceType)
+                syncReverenceAllowedField.Watch(__instance.gene);
+        }
+    }
+}
---
+++ Source/TemporarySyncWorkers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Multiplayer.API;
+using RimWorld;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    public static class TemporarySyncWorkers
+    {
+        private static bool isIGeneResourceDrainInitialized = false;
+        
+        public static void RegisterIGeneResourceDrain()
+        {
+            if (isIGeneResourceDrainInitialized)
+                return;
+            
+            MP.RegisterSyncWorker<IGeneResourceDrain>(SyncIGeneResourceDrain);
+            isIGeneResourceDrainInitialized = true;
+        }
+
+        private static void SyncIGeneResourceDrain(SyncWorker sync, ref IGeneResourceDrain resourceDrain)
+        {
+            if (sync.isWriting)
+            {
+                if (resourceDrain is Gene gene)
+                    sync.Write(gene);
+                else
+                    throw new Exception($"Unsupported {nameof(IGeneResourceDrain)} type: {resourceDrain.GetType()}");
+            }
+            else
+                resourceDrain = sync.Read<Gene>() as IGeneResourceDrain;
+        }
+    }
+}
