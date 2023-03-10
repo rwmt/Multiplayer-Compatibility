732b26089023a01e74dc964d9baae3378cc7c10a
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2023-03-10 22:38:12 +0100

    Synced door gizmos for Clean Pathfinding 2 (#281)
    Co-authored-by: Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>

---
+++ Source/Mods/CleanPathfinding2.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Clean Pathfinding 2 by Owlchemist</summary>
+    /// <see href="https://github.com/Owlchemist/clean-pathfinding"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2603765747"/>
+    [MpCompatFor("Owlchemist.CleanPathfinding")]
+    public class CleanPathfinding2
+    {
+        public CleanPathfinding2(ModContentPack mod)
+            // Switch door type gizmo
+            => MP.RegisterSyncMethod(AccessTools.DeclaredMethod("CleanPathfinding.MapComponent_DoorPathing:SwitchDoorType"));
+    }
+}
