28431abdf1c6329f86402bd3fdf49d692439ca43
Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>
2023-03-10 11:39:47 +0100

    Slight change to handling temporary sync workers
    Changes based on: https://github.com/rwmt/Multiplayer-Compatibility/pull/275#issuecomment-1463019723

---
+++ Source/MpSyncWorkers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Multiplayer.API;
+using RimWorld;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    public static class MpSyncWorkers
+    {
+        private static HashSet<Type> registeredSyncWorkers = null;
+
+        public static void Requires<T>() => Requires(typeof(T));
+
+        public static void Requires(Type type)
+        {
+            if (registeredSyncWorkers != null && registeredSyncWorkers.Contains(type))
+                return;
+
+            if (type == typeof(IGeneResourceDrain))
+                MP.RegisterSyncWorker<IGeneResourceDrain>(SyncIGeneResourceDrain);
+            else
+            {
+                Log.Error($"Trying to register SyncWorker of type {type}, but it's not supported.\n{new StackTrace(1)}");
+                return;
+            }
+
+            registeredSyncWorkers ??= new HashSet<Type>();
+            registeredSyncWorkers.Add(type);
+        }
+
+        private static void SyncIGeneResourceDrain(SyncWorker sync, ref IGeneResourceDrain resourceDrain)
+        {
+            if (sync.isWriting)
+            {
+                if (resourceDrain is Gene gene)
+                    sync.Write(gene);
+                else
+                    throw new Exception($"Unsupported {nameof(IGeneResourceDrain)} type: {resourceDrain.GetType()}");
+            }
+            else
+                resourceDrain = sync.Read<Gene>() as IGeneResourceDrain;
+        }
+    }
+}
