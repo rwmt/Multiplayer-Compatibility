6f9f0621dcc684b2b1d37198424e9269ac17e4db
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2023-05-24 04:02:52 +0200

    Added compat for Alpha Mechs (#311)
    Also reused and slightly modified the transpiler from Vanilla Races Expanded - Phytokin
    
    Co-authored-by: Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>

---
+++ Source/Mods/AlphaMechs.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Alpha Mechs by Sarg Bjornson</summary>
+    /// <see href="https://github.com/juanosarg/AlphaMechs"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2973169158"/>
+    [MpCompatFor("sarg.alphamechs")]
+    public class AlphaMechs
+    {
+        public AlphaMechs(ModContentPack mod)
+        {
+            // Fix the mod using Find.CurrentMap instead of parent.Map - in both cases it creates new lord job on current (instead of parent) map
+            // Change mech to a vanilla one if the mod mechanoid is disabled
+            MpCompat.harmony.Patch(AccessTools.DeclaredMethod("AlphaMechs.CompChangeDef:CompTick"),
+                transpiler: new HarmonyMethod(typeof(VanillaRacesPhytokin), nameof(VanillaRacesPhytokin.UseParentMap)));
+            // Hediff runs out and mech turns back hostile
+            MpCompat.harmony.Patch(AccessTools.DeclaredMethod("AlphaMechs.HediffComp_DeleteAfterTime:CompPostTick"),
+                transpiler: new HarmonyMethod(typeof(VanillaRacesPhytokin), nameof(VanillaRacesPhytokin.UseParentMap)));
+
+            // Gizmos
+            MP.RegisterSyncMethod(AccessTools.DeclaredMethod("AlphaMechs.Pawn_HemogenVat:EjectContents"));
+        }
+    }
+}
