22b837c3afb00f41670c4b93bb05ea962272ef8b
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2023-05-24 04:04:29 +0200

    Compat for Mech Framework (#313)
    Specifically used by Amplified Mobility Platform and Metal Gear Rimworld - walker gears. Possibly more.
    
    Co-authored-by: Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>

---
+++ Source/Mods/MechFramework.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Mech Framework by andery233xj</summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2826884300"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2763366485"/>
+    [MpCompatFor("Aoba.AMP")]
+    [MpCompatFor("Aoba.WalkerGears")] // 1.3, not updated yet
+    // Couldn't find any more mods using it, but more may exist.
+    public class MechFramework
+    {
+        // Just in case multiple mods using it are active
+        private static bool patchOnce = false;
+
+        public MechFramework(ModContentPack mod)
+        {
+            if (patchOnce)
+                return;
+
+            patchOnce = true;
+            LongEventHandler.ExecuteWhenFinished(LatePatch);
+        }
+
+        private static void LatePatch()
+            => MP.RegisterSyncMethod(AccessTools.DeclaredMethod("WalkerGear.WalkerGear_Core:GetOut"));
+    }
+}
