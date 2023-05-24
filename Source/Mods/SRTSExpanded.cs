46f7b482cdbb5cf55d52b1ecd25ea3bd07d66671
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2023-05-24 04:02:09 +0200

    Added compat Carryalls, slightly updated SRTS (#310)
    Co-authored-by: Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>

--- Source/Mods/SRTSExpanded.cs
+++ Source/Mods/SRTSExpanded.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using HarmonyLib;
 using Multiplayer.API;
 using RimWorld.Planet;
@@ -6,24 +8,30 @@ using Verse;
 
 namespace Multiplayer.Compat
 {
-    /// <summary>SRTS Expanded by Smash Phil, Aquamarine, Neceros, more</summary>
+    /// <summary>
+    /// <para>SRTS Expanded by Smash Phil, Aquamarine, Neceros, more</para>
+    /// <para>Carryalls | Intercontinental Transport by Nephlite</para>
+    /// </summary>
     /// <see href="https://github.com/Neceros/SRTS-Expanded"/>
     /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1845423808"/>
+    /// <see href="https://github.com/RealTelefonmast/RWCarryall"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2901034783"/>
     [MpCompatFor("smashphil.srtsexpanded")]
     [MpCompatFor("smashphil.neceros.srtsexpanded")]
     [MpCompatFor("Shashlichnik.srtsexpanded")]
+    [MpCompatFor("Nephlite.Carryalls")]
     class SRTSExpanded
     {
-        private static MethodInfo tryLaunchMethod;
+        private static FastInvokeHandler tryLaunchMethod;
         private static AccessTools.FieldRef<object, Caravan> caravanField;
         private static ISyncField bombTypeSync;
 
         public SRTSExpanded(ModContentPack mod)
         {
-            LongEventHandler.ExecuteWhenFinished(DelayedPatch);
+            LongEventHandler.ExecuteWhenFinished(() => DelayedPatch(mod.PackageId));
 
             // Refuel shuttle
-            MP.RegisterSyncDelegate(AccessTools.TypeByName("SRTS.StartUp"), "<>c__DisplayClass24_1", "<LaunchAndBombGizmosPassthrough>b__3");
+            MpCompat.RegisterLambdaDelegate("SRTS.StartUp", "LaunchAndBombGizmosPassthrough", 3);
 
             // Bombing
             MP.RegisterSyncWorker<Pair<IntVec3, IntVec3>>(SyncIntVec3Pair, typeof(Pair<IntVec3, IntVec3>));
@@ -31,21 +39,26 @@ namespace Multiplayer.Compat
             MP.RegisterSyncMethod(type, "TryLaunchBombRun");
             bombTypeSync = MP.RegisterSyncField(type, "bombType");
 
-            foreach (MethodInfo method in MpMethodUtil.GetLambda(type, "CompGetGizmosExtra", MethodType.Normal, null, 1, 2))
+            foreach (MethodInfo method in MpMethodUtil.GetLambda(type, nameof(ThingComp.CompGetGizmosExtra), MethodType.Normal, null, 1, 2))
                 MpCompat.harmony.Patch(method,
                     prefix: new HarmonyMethod(typeof(SRTSExpanded), nameof(PreSyncBombType)),
                     postfix: new HarmonyMethod(typeof(SRTSExpanded), nameof(PostSyncBombType)));
         }
 
-        private static void DelayedPatch()
+        private static void DelayedPatch(string modId)
         {
             // Launching the shuttle
             var type = AccessTools.TypeByName("SRTS.CompLaunchableSRTS");
             caravanField = AccessTools.FieldRefAccess<Caravan>(type, "carr");
-            tryLaunchMethod = AccessTools.Method(type, "TryLaunch");
+            var tryLaunch = AccessTools.Method(type, "TryLaunch");
+            tryLaunchMethod = MethodInvoker.GetHandler(tryLaunch);
 
-            MpCompat.harmony.Patch(tryLaunchMethod, prefix: new HarmonyMethod(typeof(SRTSExpanded), nameof(PreTryLaunch)));
+            MpCompat.harmony.Patch(tryLaunch, prefix: new HarmonyMethod(typeof(SRTSExpanded), nameof(PreTryLaunch)));
             MP.RegisterSyncMethod(typeof(SRTSExpanded), nameof(SyncedLaunch)).ExposeParameter(2);
+
+            // Patch stuff unique to carryalls
+            if (modId == "Nephlite.Carryalls".ToLower())
+                PatchCarryalls();
         }
 
         private static void PreSyncBombType(ThingComp __instance)
@@ -78,7 +91,7 @@ namespace Multiplayer.Compat
         private static void SyncedLaunch(ThingComp compLaunchableSrts, int destinationTile, TransportPodsArrivalAction arrivalAction, Caravan caravanMethodParameter, Caravan caravanFieldValue)
         {
             caravanField(compLaunchableSrts) = caravanFieldValue;
-            tryLaunchMethod.Invoke(compLaunchableSrts, new object[] { destinationTile, arrivalAction, caravanMethodParameter });
+            tryLaunchMethod(compLaunchableSrts, destinationTile, arrivalAction, caravanMethodParameter);
         }
 
         private static void SyncIntVec3Pair(SyncWorker sync, ref Pair<IntVec3, IntVec3> pair)
@@ -91,5 +104,65 @@ namespace Multiplayer.Compat
             else
                 pair = new Pair<IntVec3, IntVec3>(sync.Read<IntVec3>(), sync.Read<IntVec3>());
         }
+
+        #region Carryalls
+
+        // CompLaunchableSRTS
+        private static FastInvokeHandler boardShuttleMethod;
+        private static AccessTools.FieldRef<ThingComp, List<Pawn>> tmpAllowedPawnsField;
+        // Designator_AddToCarryall
+        private static Type designatorAddToCarryallType;
+        private static AccessTools.FieldRef<Designator, ThingComp> designatorLaunchableField;
+
+        private static void PatchCarryalls()
+        {
+            var type = AccessTools.TypeByName("SRTS.CompLaunchableSRTS");
+            tmpAllowedPawnsField = AccessTools.FieldRefAccess<List<Pawn>>(type, "tmpAllowedPawns");
+
+            // Rotate, self-destruct (Kirov only)
+            MpCompat.RegisterLambdaMethod(type, nameof(ThingComp.CompGetGizmosExtra), 0, 1);
+            // Add pawn
+            MpCompat.RegisterLambdaDelegate(type, nameof(ThingComp.CompFloatMenuOptions), 0);
+            // Add selected pawns
+            var boardShuttle = MpMethodUtil.GetLambda(type, nameof(ThingComp.CompMultiSelectFloatMenuOptions), lambdaOrdinal: 0);
+            boardShuttleMethod = MethodInvoker.GetHandler(boardShuttle);
+            MpCompat.harmony.Patch(boardShuttle, prefix: new HarmonyMethod(typeof(SRTSExpanded), nameof(PreAddPawns)));
+            MP.RegisterSyncMethod(typeof(SRTSExpanded), nameof(SyncedAddPawns));
+
+            // Designator
+            type = designatorAddToCarryallType = AccessTools.TypeByName("SRTS.Designator_AddToCarryall");
+            designatorLaunchableField = AccessTools.FieldRefAccess<ThingComp>(type, "launchable");
+            MP.RegisterSyncWorker<Designator>(SyncAddToCarryallDesignator, type);
+        }
+
+        private static bool PreAddPawns(ThingComp __instance, List<Pawn> ___tmpAllowedPawns)
+        {
+            // Let the method run only if it's synced call
+            if (!MP.IsInMultiplayer || MP.IsExecutingSyncCommand)
+                return true;
+
+            SyncedAddPawns(__instance, ___tmpAllowedPawns);
+            return false;
+        }
+
+        private static void SyncedAddPawns(ThingComp comp, List<Pawn> pawns)
+        {
+            ref var currentFieldValue = ref tmpAllowedPawnsField(comp);
+            // Set the list in the comp to the one we got, and store the previous value in the temp value (swap)
+            (currentFieldValue, pawns) = (pawns, currentFieldValue);
+            boardShuttleMethod(comp);
+            // Restore the original list of pawns
+            currentFieldValue = pawns;
+        }
+
+        private static void SyncAddToCarryallDesignator(SyncWorker sync, ref Designator designator)
+        {
+            if (sync.isWriting)
+                sync.Write(designatorLaunchableField(designator));
+            else
+                designator = (Designator)Activator.CreateInstance(designatorAddToCarryallType, sync.Read<ThingComp>());
+        }
+
+        #endregion
     }
 }
