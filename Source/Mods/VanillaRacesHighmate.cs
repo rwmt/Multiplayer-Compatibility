3c24ce33f0c22df6686e9e1fb60e5aba4687f1f3
SokyranTheDragon <exploder1531@gmail.com>
2023-06-22 01:53:45 +0200

    Initial compat for Vanilla Races Expanded - Highmate
    I think this should patch everything that needs patching so far. However, since the mod is in development - this may change.
    
    No links to the mod as again, it's not ready yet.

---
+++ Source/Mods/VanillaRacesHighmate.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Vanilla Races Expanded - Highmate by Oskar Potocki, Taranchuk, Sarg Bjornson</summary>
+    [MpCompatFor("vanillaracesexpanded.highmate")]
+    public class VanillaRacesHighmate
+    {
+        public VanillaRacesHighmate(ModContentPack mod)
+        {
+            // Select pregnancy approach (replaces vanilla one if one of the pawns is highmate/lowmate)
+            {
+                MpSyncWorkers.Requires<SocialCardUtility.CachedSocialTabEntry>();
+
+                MpCompat.RegisterLambdaDelegate(
+                    "VanillaRacesExpandedHighmate.SocialCardUtility_DrawPregnancyApproach_Patch", 
+                    "AddPregnancyApproachOption",
+                    Enumerable.Range(0, 4).ToArray()); // Options 0 through 3
+            }
+        }
+    }
+}
