4d35efaa3ee22b55d49471fa51472a26c9e78737
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2023-07-01 04:51:23 +0200

    Updated Vanilla Traits Expanded compat (#331)
    This should (hopefully) fix issues with the following traits: absent minded, rebel, submissive, menagerist.
    
    I did not find any issues with other traits, but it may have been due to not testing enough.

--- Source/Mods/VanillaTraitsExpanded.cs
+++ Source/Mods/VanillaTraitsExpanded.cs
@@ -1,4 +1,7 @@
-﻿using Verse;
+﻿using System.Collections;
+using HarmonyLib;
+using Multiplayer.API;
+using Verse;
 
 namespace Multiplayer.Compat
 {
@@ -8,7 +11,33 @@ namespace Multiplayer.Compat
     [MpCompatFor("VanillaExpanded.VanillaTraitsExpanded")]
     public class VanillaTraitsExpanded
     {
-        public VanillaTraitsExpanded(ModContentPack mod) =>
-            PatchingUtilities.PatchPushPopRand("VanillaTraitsExpanded.TraitsManager:GameComponentTick");
+        private static AccessTools.FieldRef<IDictionary> mapPawnsAnimalListField;
+        
+        public VanillaTraitsExpanded(ModContentPack mod)
+        {
+            // Stop doing stuff with absent minded/rebel/submissive pawns outside of synced context
+            {
+                // Basically, this method adds/removes slow work speed hediff to/from rebel/submissive pawns.
+                // On top of that, it also adds the current job to the list of forced jobs for absent minded pawns.
+                // This can cause issues when the mod operates and does stuff based on those, but they are in different state for all players.
+                // The issue is that a sync method catches this call, but all prefixes/postfixes/etc. still run.
+                MpCompat.harmony.Patch(AccessTools.DeclaredMethod("VanillaTraitsExpanded.TryTakeOrderedJob_Patch:Postfix"),
+                    prefix: new HarmonyMethod(typeof(VanillaTraitsExpanded), nameof(CancelUnlessSynced)));
+            }
+
+            // Clear cache
+            {
+                // List of animals on current map for menagerist trait
+                var field = AccessTools.DeclaredField("VanillaTraitsExpanded.ThoughtWorker_AnimalsInColony:mapPawns");
+                mapPawnsAnimalListField = AccessTools.StaticFieldRefAccess<IDictionary>(field);
+
+                MpCompat.harmony.Patch(AccessTools.DeclaredMethod(typeof(GameComponentUtility), nameof(GameComponentUtility.FinalizeInit)),
+                    postfix: new HarmonyMethod(typeof(VanillaTraitsExpanded), nameof(ClearCache)));
+            }
+        }
+
+        private static bool CancelUnlessSynced() => !MP.IsInMultiplayer || MP.IsExecutingSyncCommand;
+
+        private static void ClearCache() => mapPawnsAnimalListField().Clear();
     }
 }
