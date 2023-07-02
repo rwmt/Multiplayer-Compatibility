53479a01fcb53cf80d5fa0d607de562258b38709
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2023-07-02 19:36:46 +0200

    Added compat for Hunt for Me (#159)

---
+++ Source/Mods/HuntForMe.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Hunt for Me by aRandomKiwi</summary>
+    /// <see href="https://github.com/aRandomKiwi/Hunt-For-Me"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1593245720"/>
+    [MpCompatFor("aRandomKiwi.HuntForMe")]
+    internal class HuntForMe
+    {
+        public HuntForMe(ModContentPack mod)
+        {
+            var type = AccessTools.TypeByName("aRandomKiwi.HFM.Pawn_MindState_Patch");
+            type = AccessTools.Inner(type, "GetGizmos");
+            MpCompat.RegisterLambdaDelegate(type, "Listener", 1, 3, 5, 6, 7, 8)
+                .Take(3).ToArray().SetDebugOnly();
+
+            var typeNames = new[]
+            {
+                "aRandomKiwi.HFM.PawnColumnWorker_Hunting",
+                "aRandomKiwi.HFM.PawnColumnWorker_HuntingCanAssist",
+                "aRandomKiwi.HFM.PawnColumnWorker_PreyModeHunting",
+                "aRandomKiwi.HFM.PawnColumnWorker_SupervisedHunting",
+            };
+
+            foreach (var typeName in typeNames)
+            {
+                type = AccessTools.TypeByName(typeName);
+                MP.RegisterSyncMethod(type, "SetValue");
+            }
+        }
+    }
+}
