893c79867db2a5290000ec3caa0ec1530eea82fe
SokyranTheDragon <exploder1531@gmail.com>
2023-07-27 13:06:54 +0200

    Fixed issues with Hospitality's main tab

--- Source/Mods/Hospitality.cs
+++ Source/Mods/Hospitality.cs
@@ -22,6 +22,11 @@ namespace Multiplayer.Compat
         private static AccessTools.FieldRef<IDictionary> relatedCacheField;
         private static AccessTools.FieldRef<int> relatedCacheNextClearTickField;
 
+        // CompUtility
+        private static FastInvokeHandler compGuestMethod;
+        // Multiplayer
+        private static AccessTools.FieldRef<ISyncField[]> syncFields;
+
         public Hospitality(ModContentPack mod)
         {
             // TODO: Check Hospitality.Patches.MinifiedThing_Patch.DrawAt:Prefix, specifically the HospitalityModBase.RegisterTickAction call
@@ -60,6 +65,25 @@ namespace Multiplayer.Compat
                 MpCompat.harmony.Patch(AccessTools.DeclaredMethod(typeof(GameComponentUtility), nameof(GameComponentUtility.FinalizeInit)),
                     postfix: new HarmonyMethod(typeof(Hospitality), nameof(ClearCache)));
             }
+
+            // Sync fields
+            {
+                // Hospitality starts watching the fields way too late (inside of PawnColumnWorker_Relationship.DoCell),
+                // causing it to fail syncing in some situations. For example, shift-clicking on the header of make friends/entertain guests checkboxes.
+                syncFields = AccessTools.StaticFieldRefAccess<ISyncField[]>(AccessTools.DeclaredField("Hospitality.Multiplayer:guestFields"));
+
+                // Basically an extension method to pawn.GetComp<CompGuest>, but with extra caching and null.
+                compGuestMethod = MethodInvoker.GetHandler(AccessTools.DeclaredMethod("Hospitality.CompUtility:CompGuest"));
+
+                // Hospitality starts watching in this DoHeader is called before DoCell, so it should be good enough of a place to put it in.
+                var type = AccessTools.TypeByName("Hospitality.MainTab.PawnColumnWorker_Relationship");
+
+                MpCompat.harmony.Patch(AccessTools.DeclaredMethod(type, nameof(PawnColumnWorker.DoHeader)),
+                    prefix: new HarmonyMethod(typeof(Hospitality), nameof(PreHeader)));
+                MpCompat.harmony.Patch(AccessTools.DeclaredMethod(type, nameof(PawnColumnWorker.DoCell)),
+                    prefix: new HarmonyMethod(typeof(Hospitality), nameof(PreCell)),
+                    finalizer: new HarmonyMethod(typeof(Hospitality), nameof(PostCell)));
+            }
         }
 
         private static bool NoAlertInMp(ref AlertReport __result)
@@ -90,6 +114,46 @@ namespace Multiplayer.Compat
 
             // Hospitality.Patches.ThingFilter_Patch.Allows:bedNameToThing doesn't look like it needs clearing
             // Hospitality.Utilities.ThoughtResultCache clears the caches on load (but not when hosting new game).
+        }
+
+        // When making the PR to Hospitality, put it after MP WatchBegin in Hospitality.MainTab.MainTabWindow_Hospitality:DoWindowContents.
+        // Also remember to remove the sync field watch from Hospitality.MainTab.PawnColumnWorker_Relationship:DoCell.
+        private static void PreHeader(PawnTable table)
+        {
+            if (!MP.IsInMultiplayer)
+                return;
+            ref var fields = ref syncFields();
+            if (fields == null)
+                return;
+
+            foreach (var pawn in table.PawnsListForReading)
+            {
+                var comp = compGuestMethod(null, pawn);
+
+                if (comp != null)
+                {
+                    foreach (var field in fields)
+                        field.Watch(comp);
+                }
+            }
+        }
+
+        // Stop watching sync fields for the second time, as it breaks syncing area syncing.
+        private static void PreCell(ref ISyncField[] __state)
+        {
+            if (MP.IsInMultiplayer)
+            {
+                // Hospitality doesn't sync those if the field is null, so temporarily change it to null
+                __state = syncFields();
+                syncFields() = null;
+            }
+        }
+
+        private static void PostCell(ISyncField[] __state)
+        {
+            // Restore data to the state it was beforehand
+            if (__state != null)
+                syncFields() = __state;
         }
     }
 }
