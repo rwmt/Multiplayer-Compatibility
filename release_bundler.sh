be8181c97a0f2db07da7df4c29bf26fc5b985601
notfood <notfood@users.noreply.github.com>
2023-12-02 11:55:42 -0500

    Fix missing build from workflows; Rename

--- release_bundler.sh
+++ release_bundler.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 cd Source
-dotnet build --configuration Release
+dotnet build -c Release
+cd ../Source_Referenced
+dotnet build -c Release
 cd ..
 
 rm -rf Multiplayer-Compatibility/
 mkdir -p Multiplayer-Compatibility
 
-cp -r About Assemblies Referenced 1.3 Multiplayer-Compatibility
+cp -r About Assemblies Referenced Multiplayer-Compatibility
 
 # Zip for Github releases
 rm -f Multiplayer-Compatibility.zip
 zip -r -q Multiplayer-Compatibility.zip Multiplayer-Compatibility
 
-echo "Ok, $PWD/Multiplayer-Compatibility.zip ready for uploading to Workshop"
+echo "Ok, $PWD/Multiplayer-Compatibility.zip ready"
