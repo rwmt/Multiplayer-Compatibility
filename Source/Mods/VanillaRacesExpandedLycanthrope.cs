62b9a80a485b7727da1e789f1b7df3acc27d61f0
SokyranTheDragon <exploder1531@gmail.com>
2023-12-07 18:29:11 +0100

    Compat for Vanilla Races Expanded - Lycanthrope

---
+++ Source/Mods/VanillaRacesExpandedLycanthrope.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Vanilla Races Expanded - Lycanthrope by Oskar Potocki, Sarg Bjornson</summary>
+    /// TODO: Add links to mod workshop/github
+    [MpCompatFor("vanillaracesexpanded.lycanthrope")]
+    public class VanillaRacesExpandedLycanthrope
+    {
+        public VanillaRacesExpandedLycanthrope(ModContentPack mod)
+        {
+             AccessTools.DeclaredField("VanillaRacesExpandedLycanthrope.VanillaRacesExpandedLycanthrope_InteractionWorker_Interacted_Patch:random")
+                 .SetValue(null, PatchingUtilities.RandRedirector.Instance);
+        }
+    }
+}
