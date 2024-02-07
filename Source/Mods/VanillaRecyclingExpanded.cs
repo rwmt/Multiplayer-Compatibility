ca318f9ddc125334476ac01e3097664f362c80e2
SokyranTheDragon <exploder1531@gmail.com>
2024-02-07 19:28:00 +0100

    Compat for Vanilla Recycling Expanded

---
+++ Source/Mods/VanillaRecyclingExpanded.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using RimWorld;
+using Verse;
+
+namespace Multiplayer.Compat;
+
+/// <summary>Vanilla Recycling Expanded by OskarPotocki, Sarg Bjornson, xrushha</summary>
+[MpCompatFor("VanillaExpanded.Recycling")]
+public class VanillaRecyclingExpanded
+{
+    public VanillaRecyclingExpanded(ModContentPack mod)
+    {
+        MpCompatPatchLoader.LoadPatch(this);
+
+        var type = AccessTools.TypeByName("VanillaRecyclingExpanded.CompBiopackDissolution");
+        // Dev: Single dissolution event (0), dissolution events until destroyed (1), +25% dissolution progress
+        MpCompat.RegisterLambdaMethod(type, nameof(ThingComp.CompGetGizmosExtra), 0, 1, 2).SetDebugOnly();
+        // Dev: Set next dissolve time
+        MP.RegisterSyncDelegateLambda(type, nameof(ThingComp.CompGetGizmosExtra), 4).SetDebugOnly();
+
+        type = AccessTools.TypeByName("VanillaRecyclingExpanded.CompSuperSimpleProcessor");
+        MP.RegisterSyncMethod(type, "EjectContents");
+        // In interface only called as dev mode command
+        MP.RegisterSyncMethod(type, "DoAtomize").SetDebugOnly();
+        // Toggle auto load
+        MP.RegisterSyncMethodLambda(type, nameof(ThingComp.CompGetGizmosExtra), 1);
+        // Dev: Set next processing time
+        MP.RegisterSyncDelegateLambda(type, nameof(ThingComp.CompGetGizmosExtra), 3).SetDebugOnly();
+    }
+
+    // Stop methods from running (most likely after syncing) when it would cause errors or other issues.
+    [MpCompatPrefix("VanillaRecyclingExpanded.CompBiopackDissolution", "TriggerDissolutionEvent")]
+    private static bool PreDoAtomize(CompThingContainer __instance) => __instance.ContainedThing is { stackCount: > 0 };
+}
