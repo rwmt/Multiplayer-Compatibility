f6058f40959a85fcb2763244641225c0e1ef464e
SokyranTheDragon <exploder1531@gmail.com>
2024-02-22 18:14:26 +0100

    Updated Vanilla Furniture Expanded compat

--- Source/Mods/VanillaFurnitureExpanded.cs
+++ Source/Mods/VanillaFurnitureExpanded.cs
@@ -1,4 +1,5 @@
-﻿using Verse;
+﻿using Multiplayer.API;
+using Verse;
 
 namespace Multiplayer.Compat
 {
@@ -8,10 +9,36 @@ namespace Multiplayer.Compat
     [MpCompatFor("VanillaExpanded.VFECore")]
     internal class VanillaFurnitureExpanded
     {
+        [MpCompatSyncField("VanillaFurnitureEC.CompBinClean", "cleanupTarget")]
+        private static ISyncField cleanupTargetField = null;
+
         public VanillaFurnitureExpanded(ModContentPack mod)
         {
+            MpCompatPatchLoader.LoadPatch(this);
+
             // Uses ShouldSpawnMotesAt, so could potentially cause issues somewhere. Seems like no RNG though... Let's better be safe here.
             PatchingUtilities.PatchPushPopRand("VanillaFurnitureEC.JobDriver_PlayDarts:ThrowDart");
+        }
+
+        [MpCompatPrefix("VanillaFurnitureEC.Command_CleanupTarget", "GizmoOnGUI")]
+        private static void PreGizmoGui(ThingComp ___comp, out bool __state)
+        {
+            if (!MP.IsInMultiplayer || ___comp == null)
+            {
+                __state = false;
+                return;
+            }
+
+            __state = true;
+            MP.WatchBegin();
+            cleanupTargetField.Watch(___comp);
+        }
+
+        [MpCompatPostfix("VanillaFurnitureEC.Command_CleanupTarget", "GizmoOnGUI")]
+        private static void PostGizmoGui(bool __state)
+        {
+            if (__state)
+                MP.WatchEnd();
         }
     }
 }
