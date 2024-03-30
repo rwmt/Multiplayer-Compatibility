ff63583eb06593dacaa9f812567e4ec2892b645a
Meru <notfood@users.noreply.github.com>
2024-03-30 18:26:16 -0500

    Update TimeOfDAySwitch.cs
    fixes #434

--- Source/Mods/TimeOfDAySwitch.cs
+++ Source/Mods/TimeOfDAySwitch.cs
@@ -5,14 +5,14 @@ using Verse;
 namespace Multiplayer.Compat
 {
     /// <summary>Time of day switches by merthsoft</summary>
-    /// <see href="https://bitbucket.org/merthsoft/timeofdayswitch"/>
+    /// <see href="https://github.com/merthsoft/time-of-day-switch"/>
     /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=776114412"/>
-    [MpCompatFor("Merthsoft.TimeOfDaySwitches")]
+    [MpCompatFor("Merthsoft.TimeOfDaySwitch")]
     class TimeOfDAySwitch
     {
         public TimeOfDAySwitch(ModContentPack mod)
         {
-            MP.RegisterSyncMethod(AccessTools.Method("TimerSwitches.TimeOfDaySwitch:SetState"));
+            MP.RegisterSyncMethod(AccessTools.Method("Merthsoft.TimerSwitches.TimeOfDaySwitch:SetState"));
         }
     }
 }
