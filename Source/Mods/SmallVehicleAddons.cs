75af97bfb1148b495f634b5298668a0c7114eec8
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2025-02-10 03:36:57 +0100

    Compat for Small Vehicle Add-ons (#509)
    Only 3 gizmos, nothing major here.

---
+++ Source/Mods/SmallVehicleAddons.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat;
+
+/// <summary>Small Vehicle Add-ons by いのしし_3</summary>
+/// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=3420948947"/>
+[MpCompatFor("Inoshishi3.SmallVehicleAddons")]
+public class SmallVehicleAddons
+{
+    public SmallVehicleAddons(ModContentPack mod)
+    {
+        // Quickly add all nearby items to transferables
+        MP.RegisterSyncMethod(AccessTools.DeclaredMethod("SmallVehicleAddons.CompFastCargo:AddItemsToTheVehicleTransferables"));
+        // Honk the car horn
+        MP.RegisterSyncMethod(AccessTools.DeclaredMethod("SmallVehicleAddons.CompHonk:DoHonkHonk"));
+        // Toggle car lights on/off
+        MP.RegisterSyncMethod(AccessTools.DeclaredMethod("SmallVehicleAddons.CompLights:ToggleDeployment"));
+    }
+}
