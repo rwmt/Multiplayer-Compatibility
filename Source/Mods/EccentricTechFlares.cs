152aa85512420c2bfc5d3e1354dedf39c3116a43
Sakura_TA <Sakura_TA@163.com>
2025-03-06 16:53:25 +0800

    Added support for Eccentric Extras - Flares by Aelanna

---
+++ Source/Mods/EccentricTechFlares.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+using System.Reflection;
+using System;
+namespace Multiplayer.Compat
+{
+    /// <summary>Eccentric Extras - Flares by Aelanna</summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2552628275"/>
+    [MpCompatFor("Aelanna.EccentricTech.Flares2")]
+    internal class EccentricTechFlares
+    {
+
+        private static ConstructorInfo commandThrowIlluminatorConstructor;
+        private static Type compIlluminatorPackType;
+        public EccentricTechFlares(ModContentPack mod)
+        {
+            // RNG
+            {
+            }
+
+            // Gizmos
+            {
+                var commandThrowIlluminatorType = AccessTools.TypeByName("EccentricFlares.Command_ThrowIlluminator");
+                compIlluminatorPackType = AccessTools.TypeByName("EccentricFlares.CompIlluminatorPack");
+
+                commandThrowIlluminatorConstructor = AccessTools.DeclaredConstructor(commandThrowIlluminatorType, [compIlluminatorPackType]);
+
+                MP.RegisterSyncWorker<object>(SyncCommandThrowIlluminator, commandThrowIlluminatorType);
+                //MP.RegisterSyncWorker<ThingComp>(SyncCompIlluminatorPack, compIlluminatorPackType);
+                MP.RegisterSyncMethod(commandThrowIlluminatorType, "SelectOption");
+
+
+
+                LongEventHandler.ExecuteWhenFinished(LatePatch);
+            }
+
+        }
+        public static void LatePatch()
+        {
+            var type = AccessTools.TypeByName("EccentricFlares.CompIlluminatorPack");
+
+            MP.RegisterSyncMethod(compIlluminatorPackType, "DoStartThrow");
+            MpCompat.RegisterLambdaMethod(type, "CompGetWornGizmosExtra", 1);
+        }
+
+        private static void SyncCommandThrowIlluminator(SyncWorker sync, ref object command)
+        {
+
+            if (sync.isWriting)
+            {
+                Traverse traverse = Traverse.Create(command);
+                ThingComp comp = traverse.Field("pack").GetValue<ThingComp>();
+                sync.Write(comp);
+            }
+            else
+            {
+                command = commandThrowIlluminatorConstructor.Invoke([sync.Read<ThingComp>()]);
+            }
+        }
+        private static void SyncCompIlluminatorPack(SyncWorker sync, ref ThingComp comp)
+        {
+            if(sync.isWriting)
+            {
+                sync.Write(comp.parent as Thing);
+            } else
+            {
+                comp = sync.Read<ThingWithComps>().AllComps.Find(_comp => _comp.GetType() == compIlluminatorPackType);
+            }
+        }
+    }
+}
