93580e303e882439705f662804c66740f96e8326
SokyranTheDragon <36712560+SokyranTheDragon@users.noreply.github.com>
2025-03-25 07:39:42 +0100

    Compat for Biomes! Polluted (#527)
    Almost everything is dev mode gizmos, but there's 1 instance of System RNG there as well.

---
+++ Source/Mods/BiomesPollutedLands.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat;
+
+/// <summary>Biomes! Polluted Lands by The Biomes Mod Team</summary>
+/// <see href="https://github.com/biomes-team/BiomesPollutedLands"/>
+/// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=3390196656"/>
+[MpCompatFor("BiomesTeam.BiomesPollutedLands")]
+public class BiomesPollutedLands
+{
+    public BiomesPollutedLands(ModContentPack mod)
+    {
+        #region (Dev) gizmos
+
+        {
+            // DEV: chronic pains
+            MP.RegisterSyncMethod(AccessTools.DeclaredMethod("BMT_PollutedLands.Gene_AddHediffWithInterval:Pain")).SetDebugOnly();
+            // Dev: add or remove hediff
+            MpCompat.RegisterLambdaMethod("BMT_PollutedLands.Gene_AddOrRemoveHediff", nameof(Gene.GetGizmos), 0).SetDebugOnly();
+            // Dev: blood vomit
+            MP.RegisterSyncMethod(AccessTools.DeclaredMethod("BMT_PollutedLands.Gene_BloodExplusion:Vomit")).SetDebugOnly();
+            // Dev: try hunt for food
+            MpCompat.RegisterLambdaMethod("BMT_PollutedLands.Gene_CarrionMetabolism", nameof(Gene.GetGizmos), 0).SetDebugOnly();
+            // Dev: heal permanent wound
+            MP.RegisterSyncMethod(AccessTools.DeclaredMethod("BMT_PollutedLands.Gene_MoltingRegeneration:TryHealWound")).SetDebugOnly();
+            // Dev: release gas
+            MpCompat.RegisterLambdaMethod("BMT_PollutedLands.Gene_ToxspewingPores", nameof(Gene.GetGizmos), 0).SetDebugOnly();
+        }
+
+        #endregion
+
+        #region RNG
+
+        {
+            PatchingUtilities.PatchSystemRand("BMT_PollutedLands.Hediff_Mutapox:Tick");
+        }
+
+        #endregion
+    }
+}
