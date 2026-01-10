2d4d1941bd98679adc6e0e7659f6732556a1980f
Chillayy <shemar.pradia@gmail.com>
2026-01-10 03:30:39 -0600

    fixed runandgun pissing me off

--- Source/Mods/RunandGun.cs
+++ Source/Mods/RunandGun.cs
@@ -5,19 +5,34 @@ using Multiplayer.API;
 
 namespace Multiplayer.Compat
 {
-    /// <summary>RunAndGun by roolo</summary>
+    /// <summary>RunAndGun by roolo, continued by MemeGoddess</summary>
     /// <see href="https://github.com/rheirman/RunAndGun"/>
+    /// <see href="https://github.com/MemeGoddess/RunAndGun-continued"/>
     /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1204108550"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=3070109648"/>
     [MpCompatFor("roolo.RunAndGun")]
+    [MpCompatFor("MemeGoddess.RunAndGun")]
     class RunandGun
     {
         public RunandGun(ModContentPack mod)
         {
-            Type type = AccessTools.TypeByName("RunAndGun.Harmony.Pawn_DraftController_GetGizmos_Patch");
+            var type = AccessTools.TypeByName("RunAndGun.Harmony.Pawn_DraftController_GetGizmos_Patch");
+            if (type != null)
+            {
+                MP.RegisterSyncDelegateLambda(type, "Postfix", 1);
+            }
 
-            MP.RegisterSyncDelegateLambda(type, "Postfix", 1);
+            type = AccessTools.TypeByName("RunAndGun.Utilities.WorldComponent_ToggleData");
+            if (type != null)
+            {
+                MP.RegisterSyncMethod(type, "SetRunAndGun");
+            }
 
-            PatchingUtilities.PatchSystemRand("RunAndGun.Harmony.MentalStateHandler_TryStartMentalState:shouldRunAndGun", false);
+            var mentalStateMethod = AccessTools.Method("RunAndGun.Harmony.MentalStateHandler_TryStartMentalState:shouldRunAndGun");
+            if (mentalStateMethod != null)
+            {
+                PatchingUtilities.PatchSystemRand("RunAndGun.Harmony.MentalStateHandler_TryStartMentalState:shouldRunAndGun", false);
+            }
         }
     }
 }
