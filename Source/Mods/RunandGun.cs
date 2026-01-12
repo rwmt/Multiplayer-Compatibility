b2ebd88aaed83a9cb590b09067ff089b72944b80
Chillayy <134171113+Chillayy@users.noreply.github.com>
2026-01-12 04:46:14 -0600

    changed to source

--- Source/Mods/RunandGun.cs
+++ Source/Mods/RunandGun.cs
@@ -5,34 +5,19 @@ using Multiplayer.API;
 
 namespace Multiplayer.Compat
 {
-    /// <summary>RunAndGun by roolo, continued by MemeGoddess</summary>
+    /// <summary>RunAndGun by roolo</summary>
     /// <see href="https://github.com/rheirman/RunAndGun"/>
-    /// <see href="https://github.com/MemeGoddess/RunAndGun-continued"/>
     /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1204108550"/>
-    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=3070109648"/>
     [MpCompatFor("roolo.RunAndGun")]
-    [MpCompatFor("MemeGoddess.RunAndGun")]
     class RunandGun
     {
         public RunandGun(ModContentPack mod)
         {
-            var type = AccessTools.TypeByName("RunAndGun.Harmony.Pawn_DraftController_GetGizmos_Patch");
-            if (type != null)
-            {
-                MP.RegisterSyncDelegateLambda(type, "Postfix", 1); // one fucking number, i cant believe this shit.
-            }
+            Type type = AccessTools.TypeByName("RunAndGun.Harmony.Pawn_DraftController_GetGizmos_Patch");
 
-            type = AccessTools.TypeByName("RunAndGun.Utilities.WorldComponent_ToggleData");
-            if (type != null)
-            {
-                MP.RegisterSyncMethod(type, "SetRunAndGun");
-            }
+            MP.RegisterSyncDelegateLambda(type, "Postfix", 1);
 
-            var mentalStateMethod = AccessTools.Method("RunAndGun.Harmony.MentalStateHandler_TryStartMentalState:shouldRunAndGun");
-            if (mentalStateMethod != null)
-            {
-                PatchingUtilities.PatchSystemRand("RunAndGun.Harmony.MentalStateHandler_TryStartMentalState:shouldRunAndGun", false);
-            }
+            PatchingUtilities.PatchSystemRand("RunAndGun.Harmony.MentalStateHandler_TryStartMentalState:shouldRunAndGun", false);
         }
     }
 }
