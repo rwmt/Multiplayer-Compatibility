adf3dbef228d4f010fcff8d5dc1979a41d64d17d
Sakura-TA <Sakura_TA@163.com>
2026-04-24 05:15:38 +0800

    Added compat to Intimacy - Socio Butterfly by turkler

---
+++ Source/Mods/RecreationalSexWithEuterpe.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Intimacy - Socio Butterfly by turkler</summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=3630896210"/>
+    [MpCompatFor("lovelydovey.recreation.witheuterpe")]
+    public class RecreationalSexWithEuterpe
+    {
+        public RecreationalSexWithEuterpe(ModContentPack mod)
+        {
+            var type = AccessTools.TypeByName("RecreationalSexWithEuterpe.PawnColumnWorker_MealTime");
+            MpCompat.RegisterLambdaDelegate(type, "GenerateDropdownElements", 0);
+
+            type = AccessTools.TypeByName("RecreationalSexWithEuterpe.HediffComp_Birthday");
+            MpCompat.RegisterLambdaMethod(type, "CompGetGizmos", 1);
+        }
+    }
+}
