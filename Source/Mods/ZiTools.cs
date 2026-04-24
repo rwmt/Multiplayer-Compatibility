0194f8f0a6b735cbe3b0db7461516da9496c03cd
Sakura-TA <Sakura_TA@163.com>
2026-04-24 12:15:16 +0800

    Fix invalid List cast

--- Source/Mods/ZiTools.cs
+++ Source/Mods/ZiTools.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using Multiplayer.API;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Verse;
 
@@ -12,10 +13,18 @@ namespace Multiplayer.Compat
     [MpCompatFor("MaxZiCode.dws.ZiTools")]
     public class ZiTools
     {
-        static object FavChange;
+        static object FavChange = null;
         static MethodInfo GetObjectsDatabase;
         static PropertyInfo PropFavList;
+
+        static MethodInfo ListContainsMethod;
+        static MethodInfo ListAddMethod;
+        static MethodInfo ListRemoveMethod;
         public ZiTools(ModContentPack mod)
+        {
+            LongEventHandler.ExecuteWhenFinished(LatePatch);
+        }
+        void LatePatch()
         {
             var type = AccessTools.TypeByName("ZiTools.ZiTools_GameComponent");
             GetObjectsDatabase = AccessTools.Method(type, "GetObjectsDatabase");
@@ -28,14 +37,19 @@ namespace Multiplayer.Compat
                 postfix: new HarmonyMethod(typeof(ZiTools), nameof(PostDoWindowContents))
                 );
             MpCompat.harmony.Patch(AccessTools.Method(type, "DrawObjectsList"),
-                postfix: new HarmonyMethod(typeof(ZiTools),nameof(PostDrawObjectsList))
+                postfix: new HarmonyMethod(typeof(ZiTools), nameof(PostDrawObjectsList))
                 );
 
             MP.RegisterSyncMethod(AccessTools.Method(typeof(ZiTools), nameof(ZiTools.SyncedRemoveFromFav)));
             MP.RegisterSyncMethod(AccessTools.Method(typeof(ZiTools), nameof(ZiTools.SyncedAddToFav)));
+
+            type = AccessTools.TypeByName("ZiTools.DBUnit");
+            ListContainsMethod = AccessTools.Method(typeof(List<>).MakeGenericType(type), "Contains");
+            ListAddMethod = AccessTools.Method(typeof(List<>).MakeGenericType(type), "Add");
+            ListRemoveMethod = AccessTools.Method(typeof(List<>).MakeGenericType(type), "Remove");
         }
 
-        public static void PostDrawObjectsList(ref object favChange)
+        public static void PostDrawObjectsList(ref IExposable favChange)
         {
             if (favChange != null)
             {
@@ -46,26 +60,40 @@ namespace Multiplayer.Compat
         {
             if(FavChange != null)
             {
-                if (!GetFavList().Contains(FavChange))
+                var list = GetFavList();
+                if(list == null)
+                {
+                    Log.Warning("ZiTools have no Favlist now");
+                    FavChange = null;
+                    return;
+                }
+                if (!(bool)ListContainsMethod.Invoke(list, [FavChange]))
+                {
+                    ListAddMethod.Invoke(list, [FavChange]);
                     SyncedRemoveFromFav(FavChange);
+                }
                 else
+                {
+                    ListRemoveMethod.Invoke(list, [FavChange]);
                     SyncedAddToFav(FavChange);
-
+                }
                 FavChange = null;
             }
         }
-        static List<object> GetFavList()
+        static object GetFavList()
         {
             object odb = GetObjectsDatabase.Invoke(null, null);
-            return PropFavList.GetValue(odb) as List<object>;
+            var list = PropFavList?.GetValue(odb);
+
+            return list;
         }
         static void SyncedRemoveFromFav(object favChange)
         {
-            GetFavList().Remove(favChange);
+            ListRemoveMethod.Invoke(GetFavList(), [FavChange]);
         }
         static void SyncedAddToFav(object favChange)
         {
-            GetFavList().Add(favChange);
+            ListAddMethod.Invoke(GetFavList(), [FavChange]);
         }
     }
 }
