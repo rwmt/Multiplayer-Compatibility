4f60230fbfdb432b7bc39df31625f697ee441b76
Sakura-TA <52643135+Sakura-TA@users.noreply.github.com>
2026-05-07 18:47:11 +0800

    Finish works on ZiTools (#595)
    * DontSync when adding Marks
    
    * Simplify generic methods calls and use different way to approach syncing the favoritelist

--- Source/Mods/ZiTools.cs
+++ Source/Mods/ZiTools.cs
@@ -1,9 +1,11 @@
 ﻿using HarmonyLib;
 using Multiplayer.API;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Verse;
+using static UnityEngine.UI.CanvasScaler;
 
 namespace Multiplayer.Compat
 {
@@ -16,21 +18,26 @@ namespace Multiplayer.Compat
         static object FavChange = null;
         static MethodInfo GetObjectsDatabase;
         static PropertyInfo PropFavList;
+        static FieldInfo UnitsDict;
 
-        static MethodInfo ListContainsMethod;
-        static MethodInfo ListAddMethod;
-        static MethodInfo ListRemoveMethod;
+        static FieldInfo DontSync;
+
+        static PropertyInfo PropLabel;
+
         public ZiTools(ModContentPack mod)
         {
             LongEventHandler.ExecuteWhenFinished(LatePatch);
         }
         void LatePatch()
         {
+            DontSync = AccessTools.Field(AccessTools.TypeByName("Multiplayer.Client.Multiplayer"), "dontSync");
+
             var type = AccessTools.TypeByName("ZiTools.ZiTools_GameComponent");
             GetObjectsDatabase = AccessTools.Method(type, "GetObjectsDatabase");
 
             type = AccessTools.TypeByName("ZiTools.ObjectsDatabase");
             PropFavList = AccessTools.Property(type, "UnitsInFavourites");
+            UnitsDict = AccessTools.Field(type, "unitsDict");
 
             type = AccessTools.TypeByName("ZiTools.ObjectSeeker_Window");
             MpCompat.harmony.Patch(AccessTools.Method(type, "DoWindowContents"),
@@ -40,14 +47,53 @@ namespace Multiplayer.Compat
                 postfix: new HarmonyMethod(typeof(ZiTools), nameof(PostDrawObjectsList))
                 );
 
-            MP.RegisterSyncMethod(AccessTools.Method(typeof(ZiTools), nameof(ZiTools.SyncedRemoveFromFav)));
-            MP.RegisterSyncMethod(AccessTools.Method(typeof(ZiTools), nameof(ZiTools.SyncedAddToFav)));
 
+            type = AccessTools.TypeByName("ZiTools.MapMarksManager");
+            MpCompat.harmony.Patch(AccessTools.Method(type, "SetMarks"),
+                prefix: new HarmonyMethod(typeof(MapMarksManager_Marks_Patch), nameof(MapMarksManager_Marks_Patch.Prefix)),
+                finalizer: new HarmonyMethod(typeof(MapMarksManager_Marks_Patch), nameof(MapMarksManager_Marks_Patch.Finalizer))
+                );
+            MpCompat.harmony.Patch(AccessTools.Method(type, "RemoveMarks"),
+                prefix: new HarmonyMethod(typeof(MapMarksManager_Marks_Patch), nameof(MapMarksManager_Marks_Patch.Prefix)),
+                finalizer: new HarmonyMethod(typeof(MapMarksManager_Marks_Patch), nameof(MapMarksManager_Marks_Patch.Finalizer))
+                );
+
+            //DBUnit
             type = AccessTools.TypeByName("ZiTools.DBUnit");
-            ListContainsMethod = AccessTools.Method(typeof(List<>).MakeGenericType(type), "Contains");
-            ListAddMethod = AccessTools.Method(typeof(List<>).MakeGenericType(type), "Add");
-            ListRemoveMethod = AccessTools.Method(typeof(List<>).MakeGenericType(type), "Remove");
+            //MP.RegisterSyncWorker<object>(SyncDBUnit, type);
+            PropLabel = AccessTools.Property(type, "Label");
+
+            MP.RegisterSyncMethod(typeof(ZiTools), nameof(ZiTools.SyncedRemoveFromFav));
+            MP.RegisterSyncMethod(typeof(ZiTools), nameof(ZiTools.SyncedAddToFav));
+
         }
+        //static void SyncDBUnit(SyncWorker sync, ref object obj)
+        //{
+        //    if(sync.isWriting)
+        //    {
+        //        sync.Write(PropLabel.GetValue(obj) as string);
+        //    } 
+        //    else
+        //    {
+        //        var label = sync.Read<string>();
+        //        GetUnitByLabel(label, ref obj);
+        //    }
+        //}
+
+        static class MapMarksManager_Marks_Patch
+        {
+            static object preDontSync;
+            public static void Prefix()
+            {
+                preDontSync = DontSync.GetValue(null);
+                DontSync.SetValue(null, true);
+            }
+            public static void Finalizer()
+            {
+                DontSync.SetValue(null, preDontSync);
+            }
+        }
+
 
         public static void PostDrawObjectsList(ref IExposable favChange)
         {
@@ -60,40 +106,54 @@ namespace Multiplayer.Compat
         {
             if(FavChange != null)
             {
-                var list = GetFavList();
+                IList list = GetFavList();
                 if(list == null)
                 {
                     Log.Warning("ZiTools have no Favlist now");
                     FavChange = null;
                     return;
                 }
-                if (!(bool)ListContainsMethod.Invoke(list, [FavChange]))
+                if (!list.Contains(FavChange))
                 {
-                    ListAddMethod.Invoke(list, [FavChange]);
-                    SyncedRemoveFromFav(FavChange);
+                    list.Add(FavChange);
+                    SyncedRemoveFromFav(PropLabel.GetValue(FavChange) as string);
                 }
                 else
                 {
-                    ListRemoveMethod.Invoke(list, [FavChange]);
-                    SyncedAddToFav(FavChange);
+                    list.Remove(FavChange);
+                    SyncedAddToFav(PropLabel.GetValue(FavChange) as string);
                 }
                 FavChange = null;
             }
         }
-        static object GetFavList()
+        static object GetUnitByLabel(string label)
         {
             object odb = GetObjectsDatabase.Invoke(null, null);
-            var list = PropFavList?.GetValue(odb);
+            IDictionary unitsDict = UnitsDict.GetValue(odb) as IDictionary;
+
+            foreach(var obj in unitsDict.Values)
+                if (PropLabel.GetValue(obj) as string == label)
+                    return obj;
+            return null;
+        }
+        static IList GetFavList()
+        {
+            object odb = GetObjectsDatabase.Invoke(null, null);
+            IList list = PropFavList?.GetValue(odb) as IList;
 
             return list;
         }
-        static void SyncedRemoveFromFav(object favChange)
+        static void SyncedRemoveFromFav(string favChangeLabel)
         {
-            ListRemoveMethod.Invoke(GetFavList(), [FavChange]);
+            var unit = GetUnitByLabel(favChangeLabel);
+            if(unit != null)
+                GetFavList().Remove(unit);
         }
-        static void SyncedAddToFav(object favChange)
+        static void SyncedAddToFav(string favChangeLabel)
         {
-            ListAddMethod.Invoke(GetFavList(), [FavChange]);
+            var unit = GetUnitByLabel(favChangeLabel);
+            if (unit != null)
+                GetFavList().Add(unit);
         }
     }
 }
