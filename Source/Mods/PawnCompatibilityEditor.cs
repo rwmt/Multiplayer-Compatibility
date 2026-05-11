7a042f6bae05c2682e2ba0b9369778d2ce607460
notfood <notfood@users.noreply.github.com>
2026-05-10 19:20:48 -0500

    Add compat for PawnCompatibilityEditor

---
+++ Source/Mods/PawnCompatibilityEditor.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Pawn Compatibility Editor by Jomar</summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=3692227939"/>
+    [MpCompatFor("jomar.pawncompatibilityeditor")]
+    public class PawnCompatibilityEditor
+    {
+        public PawnCompatibilityEditor(ModContentPack mod)
+        {
+            var type = AccessTools.TypeByName("CompatibilityEditor.CompatibilityOverrideStore");
+            MP.RegisterSyncMethod(type, "SetOverride");
+            MP.RegisterSyncMethod(type, "RemoveOverride");
+        }
+    }
+}
