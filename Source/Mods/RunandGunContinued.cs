8e0bb069cd2dfe83048b361296eee920f0e0d5bd
Grntrenchman <grntrenchman@gmail.com>
2026-07-30 13:21:54 -0700

    Add RunandGun Continued as separate patch.
    Adding a new file to patch RunandGun Continued, since it's updated patch will break RunandGun compatibility.

---
+++ Source/Mods/RunandGunContinued.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Verse;
+using HarmonyLib;
+using Multiplayer.API;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>RunAndGun Continued</summary>
+    /// <see href="https://github.com/MemeGoddess/RunAndGun"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=3562365100"/>
+    [MpCompatFor("memegoddess.RunAndGun")]
+    class RunandGunContinued
+    {
+        public RunandGunContinued(ModContentPack mod)
+        {
+            MpCompat.RegisterLambdaDelegate("RunAndGun.Harmony.Pawn_DraftController_GetGizmos_Patch", "Postfix", 1);
+            PatchingUtilities.PatchUnityRand("RunAndGun.Harmony.MentalStateHandler_TryStartMentalState:shouldRunAndGun", false);
+        }
+    }
+}
