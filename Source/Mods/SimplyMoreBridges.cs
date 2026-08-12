85529d8bae96739769f7e80975bfbb1790cb19aa
Yeosuwu <90856101+Yeosuwu@users.noreply.github.com>
2026-08-12 14:10:35 +0900

    Update SimplyMoreBridges.cs

--- Source/Mods/SimplyMoreBridges.cs
+++ Source/Mods/SimplyMoreBridges.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Reflection;
 
 using HarmonyLib;
@@ -8,14 +8,14 @@ namespace Multiplayer.Compat
 {
     /// <summary>Simply More Bridges by Lanilor</summary>
     /// <see href="https://github.com/emipa606/SimplyMoreBridges"/>
-    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2012484790"/>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=3560458000"/>
     [MpCompatFor("Mlie.SimplyMoreBridges")]
     class SimplyMoreBridgesCompat
     {
         public SimplyMoreBridgesCompat(ModContentPack mod)
         {
             MpCompat.harmony.Patch(
-                AccessTools.Method("SimplyMoreBridges.GenerateBridges:GenerateBridgeDef"),
+                AccessTools.Method("SimplyMoreBridges.GenerateBridges:generateBridgeDef"),
                 postfix: new HarmonyMethod(typeof(SimplyMoreBridgesCompat), nameof(GenerateBridgeDefPostfix))
                 );
         }
