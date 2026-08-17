091898f46b5c0bb82ce81741da0facd8109f890d
specnazere <gamer39872987z@mail.ru>
2026-08-18 03:39:24 +0700

    Create RH2FactionVOID.cs

---
+++ Source/Mods/RH2FactionVOID.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    // Fixes ONLY ChickenCorpses.dll method MakeMotes
+    // There can be more to repair, but only this caused desyncs in our game
+    [MpCompatFor("RH2.Faction.VOID")]
+    public class ChickenCorpsesCompat
+    {
+        public ChickenCorpsesCompat(ModContentPack mod)
+        {
+            var type = AccessTools.TypeByName("ChickenCorpses.CompDecayAfterDelay");
+            PatchingUtilities.PatchPushPopRand(AccessTools.Method(type, "MakeMotes"));
+        }
+    }
+}
