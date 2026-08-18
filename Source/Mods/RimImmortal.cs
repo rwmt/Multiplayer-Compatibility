e95b3882d0f057938ef0c5070942c28114dbf2ac
specnazere <gamer39872987z@mail.ru>
2026-08-19 03:02:16 +0700

    Rimimmortal toggable abilities fix

---
+++ Source/Mods/RimImmortal.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    [MpCompatFor("RI.RimImmortal.Core")]
+    public class RimImmortalCompat
+    {
+        public RimImmortalCompat(ModContentPack mod)
+        {
+            var type = AccessTools.TypeByName("WhoXiuXian.Abilities.CompAbilityEffect_ToggleHediff"); 
+            MP.RegisterSyncDelegate(type, "<>c__DisplayClass8_0", "<GetGizmos>b__2");
+        }
+    }
+}
